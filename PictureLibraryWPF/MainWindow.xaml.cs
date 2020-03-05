7f7f7038da20a361c977eacc51a952efa99d7209
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-05 18:41:51 +0100

    Add Close Button binding.

--- PictureLibraryWPF/MainWindow.xaml.cs
+++ PictureLibraryWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PictureLibraryViewModel;
 
 namespace PictureLibraryWPF
 {
@@ -23,6 +24,7 @@ namespace PictureLibraryWPF
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new PictureLibraryViewModel.MainWindowViewModel();
         }
     }
 }
