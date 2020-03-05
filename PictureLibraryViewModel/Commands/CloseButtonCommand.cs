b91a652b410d0b8e26d12288b699053852461c98
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-05 18:18:06 +0100

    Add CloseButton command.

---
+++ PictureLibraryViewModel/Commands/CloseButtonCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PictureLibraryViewModel.Commands
+{
+    public class CloseButtonCommand : ICommand
+    {
+        private MainWindowViewModel viewModel;
+
+        public CloseButtonCommand(MainWindowViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        #region ICommand Members
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return viewModel.CanClose;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.Close();
+        }
+
+        #endregion
+    }
+}
