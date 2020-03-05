7fe2a51d2272adf085d77bfd264565d96009b654
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-05 23:28:23 +0100

    Add autofac container.

---
+++ PictureLibraryViewModel/ContainerConfig.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryViewModel
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainWindowViewModel>().As<IMainWindowViewModel>();
+
+            return builder.Build();
+        }
+    }
+}
