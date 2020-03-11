12f59d7c19c0f38056c48f98e5bf10faac999b0d
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-11 13:42:07 +0100

    Add IRepository interface

---
+++ PictureLibraryModel/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryModel
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
