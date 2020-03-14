55ba28e00cee3f78e20a8d9c529c61d97770b18d
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-14 14:54:36 +0100

    Add ICloudRepository

---
+++ PictureLibraryModel/Repositories/ICloudRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryModel.Repositories
+{
+    public interface ICloudRepository<T> where T : class
+    {
+        T GetFromCloud(int id);
+        IEnumerable<T> GetAllFromCloud();
+
+        void AddToCloud(T entity);
+        void AddRangeToCloud(IEnumerable<T> entities);
+
+        void RemoveFromCloud(T entity);
+        void RemoveRangeFromCloud(IEnumerable<T> entities);
+    }
+}
