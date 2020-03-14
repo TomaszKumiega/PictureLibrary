2fea292b86b60db49bcf84a1c4b57841f6c2694a
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-14 13:54:25 +0100

    Add context to repository

--- PictureLibraryModel/Repositories/Repository.cs
+++ PictureLibraryModel/Repositories/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,13 @@ namespace PictureLibraryModel.Repositories
 {
     public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly DbContext context;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
         public void Add(T entity)
         {
             throw new NotImplementedException();
