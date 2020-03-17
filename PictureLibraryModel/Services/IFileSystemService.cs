4ddc83fa1a2e696dc1d490dda5215006b23365f7
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-17 15:12:33 +0100

    Add filesystem service interface

---
+++ PictureLibraryModel/Services/IFileSystemService.cs
@@ -0,0 +1,50 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Services
+{
+    /// <summary>
+    /// Defines file system control logic
+    /// </summary>
+    public interface IFileSystemService
+    {
+        /// <summary>
+        /// Containes path of catalog, object is currently operating on
+        /// </summary>
+        string CurrentPath { get; }
+
+        /// <summary>
+        /// Provides a list of catalogs in current directory as well as subcatalogs
+        /// </summary>
+        /// <returns></returns>
+        List<string> GetAllCatalogsPathsList();
+
+        /// <summary>
+        /// Provides a list of image files in current directory
+        /// </summary>
+        /// <returns></returns>
+        List<ImageFile> GetAllImageFiles();
+
+        /// <summary>
+        /// Copies file to the specified destination
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="destinationPath"></param>
+        void CopyFile(string filePath, string destinationPath);
+
+        /// <summary>
+        /// Moves file to the specified destination 
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="destinationPath"></param>
+        void MoveFile(string filePath, string destinationPath);
+
+        /// <summary>
+        /// Permanently removes file 
+        /// </summary>
+        /// <param name="filePath"></param>
+        void RemoveFile(string filePath);
+    }
+}
