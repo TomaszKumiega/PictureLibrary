6c87724e0a62319a700aa163417b5cd8f129d227
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-17 15:26:02 +0100

    Add filesystem service class

---
+++ PictureLibraryModel/Services/FileSystemService.cs
@@ -0,0 +1,43 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace PictureLibraryModel.Services
+{
+    public class FileSystemService : IFileSystemService
+    {
+        public string CurrentPath { get; }
+
+        public FileSystemService()
+        {
+            CurrentPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        }
+
+        public void CopyFile(string filePath, string destinationPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetAllCatalogsPathsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ImageFile> GetAllImageFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveFile(string filePath, string destinationPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
