8d7454f21b87657727a2a2f80eba06095cb6c75e
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-19 23:36:32 +0100

    Add missing comments

--- PictureLibraryViewModel/ViewModels/IFileSystemViewModel.cs
+++ PictureLibraryViewModel/ViewModels/IFileSystemViewModel.cs
@@ -7,11 +7,18 @@ namespace PictureLibraryViewModel.ViewModels
 {
     public interface IFileSystemViewModel
     {
+        /// <summary>
+        /// Contains a path of directory object is currently operating on
+        /// </summary>
         string CurrentDirectory { get; }
+        /// <summary>
+        /// Contains a list of directories names
+        /// </summary>
         List<string> Directories { get; }
+        /// <summary>
+        /// Contains a list of ImageFiles from current directory
+        /// </summary>
         List<ImageFile> ImageFiles { get; }
-        
-
-
+       
     }
 }
