e0e265156a76e56d6e8edcb7ffe302d451904006
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-21 02:17:15 +0100

    Add filesystem viewmodel class

---
+++ PictureLibraryViewModel/ViewModels/FileSystemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModels
+{
+    public class FileSystemViewModel : IFileSystemViewModel
+    {
+        public string CurrentDirectory => throw new NotImplementedException();
+
+        public ObservableCollection<Drive> Drives => throw new NotImplementedException();
+    }
+}
