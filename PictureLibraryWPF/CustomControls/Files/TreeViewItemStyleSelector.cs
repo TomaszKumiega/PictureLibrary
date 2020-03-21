70b39253bab6b769fb5120bdac4e65a4c0ecb0fc
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-21 05:09:35 +0100

    Add style selector class

---
+++ PictureLibraryWPF/CustomControls/Files/TreeViewItemStyleSelector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace PictureLibraryWPF.CustomControls.Files
+{
+    public class TreeViewItemStyleSelector : StyleSelector 
+    {
+    }
+}
