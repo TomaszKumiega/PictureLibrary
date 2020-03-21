83387124b0a47128ba688a0ca61860ced4da3b5e
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-21 01:53:57 +0100

    Add children property in directory class

--- PictureLibraryModel/Model/Directory.cs
+++ PictureLibraryModel/Model/Directory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace PictureLibraryModel.Model
@@ -8,11 +9,13 @@ namespace PictureLibraryModel.Model
     {
         public string FullPath { get; }
         public string Name { get; }
+        public ObservableCollection<object> Children { get; }
 
         public Directory(string fullPath, string name)
         {
             FullPath = fullPath;
             Name = name;
+            this.Children = new ObservableCollection<object>();
         }
     }
 }
