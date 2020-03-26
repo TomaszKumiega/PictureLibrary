84437199e5cde665a586d2f05cd48f07bc913fcf
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-03-26 18:12:18 +0100

    Remove a project

---
+++ PictureLibraryAlbumsManager/IOClasses/IImagesIO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PictureLibraryModel.
+namespace PictureLibraryAlbumsManager.IOClasses
+{
+    public interface IImagesIO
+    {
+        void 
+    }
+}
