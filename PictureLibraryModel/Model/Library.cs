1b2c82be57394016e1de5cd6ec6f6e522a17a46e
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-04-03 00:07:23 +0200

    Add library model class

---
+++ PictureLibraryModel/Model/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Model
+{
+    public class Library
+    {
+        public string Name { get; set; }
+        public List<Album> Albums { get; set; }
+    }
+}
