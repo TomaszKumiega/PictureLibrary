ded94565505adc9eaabbd2b8a91b44204461305e
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-04-05 08:25:48 +0200

    Add OnSelectedItemChanged method in FilesTree class

--- PictureLibraryWPF/CustomControls/Files/FilesTree.xaml.cs
+++ PictureLibraryWPF/CustomControls/Files/FilesTree.xaml.cs
@@ -1,5 +1,8 @@
-﻿using PictureLibraryViewModel.ViewModels;
+﻿using System;
+using System.Windows;
+using PictureLibraryViewModel.ViewModels;
 using System.Windows.Controls;
+using PictureLibraryModel.Model;
 
 namespace PictureLibraryWPF.CustomControls.Files
 {
@@ -12,6 +15,24 @@ namespace PictureLibraryWPF.CustomControls.Files
         {
             InitializeComponent();
             DataContext = fileSystemViewModel;
+        }
+
+        private void FilesTreeView_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var viewModel = DataContext as IFileSystemViewModel;
+
+            try
+            {
+                if (this.FilesTreeView.SelectedItem is Directory)
+                    viewModel.CurrentDirectory = (this.FilesTreeView.SelectedItem as Directory).FullPath;
+                else if (this.FilesTreeView.SelectedItem is Drive)
+                    viewModel.CurrentDirectory = (this.FilesTreeView.SelectedItem as Drive).FullPath;
+            }
+            catch 
+            {
+                //TODO add logger
+            }
+
         }
     }
 }
