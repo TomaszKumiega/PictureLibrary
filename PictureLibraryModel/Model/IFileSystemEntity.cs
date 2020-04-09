4b5a4b24fa311be5daa9bf7448cae5c12886ab3c
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-04-09 19:08:55 +0200

    Add filesystem entity interface

---
+++ PictureLibraryModel/Model/IFileSystemEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace PictureLibraryModel.Model
+{
+    public interface IFileSystemEntity
+    {
+        Image Icon { get; }
+        string Name { get; }
+        string FullPath { get; }
+    }
+}
