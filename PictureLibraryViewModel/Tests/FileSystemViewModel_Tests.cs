4e925c4f22e4828f54d6b572c5c743a7e6e24c6c
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-04-12 01:10:37 +0200

    Add FileSystemViewModel.Drives.Get unit test

--- PictureLibraryViewModel/Tests/FileSystemViewModel_Tests.cs
+++ PictureLibraryViewModel/Tests/FileSystemViewModel_Tests.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using Moq;
+using PictureLibraryModel.Model;
+using PictureLibraryModel.Services;
+using PictureLibraryViewModel.ViewModels;
+using Xunit;
+using Directory = PictureLibraryModel.Model.Directory;
 
 namespace PictureLibraryViewModel.Tests
 {
     public class FileSystemViewModel_Tests
     {
+        [Fact]
+        public async Task DrivesGet_ShouldReturnListOfDrivesAfterConstruction()
+        {
+            var mock = new Mock<IFileSystemService>();
+            mock.Setup(x => x.GetDrives())
+                .Returns(GetDrivesSample());
+
+            var fileSystemVM = new FileSystemViewModel(mock.Object);
+            Task.WaitAll(fileSystemVM.Initialize());
+
+            Assert.True(fileSystemVM.Drives.Count == 5);
+        }
+
+        private ObservableCollection<Drive> GetDrivesSample()
+        {
+            var drives = new ObservableCollection<Drive>();
+
+            drives.Add(new Drive("Drive1", new FileSystemService()));
+            drives.Add(new Drive("Drive2", new FileSystemService()));
+            drives.Add(new Drive("Drive3", new FileSystemService()));
+            drives.Add(new Drive("Drive4", new FileSystemService()));
+            drives.Add(new Drive("Drive5", new FileSystemService()));
+
+            return drives;
+        }
     }
 }
