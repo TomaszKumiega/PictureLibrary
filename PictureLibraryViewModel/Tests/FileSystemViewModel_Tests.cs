1bdab6abf8d290d37eca07cb076429509487bff1
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-04-12 02:21:43 +0200

    Add CurrentDirectoryContent property unit test

--- PictureLibraryViewModel/Tests/FileSystemViewModel_Tests.cs
+++ PictureLibraryViewModel/Tests/FileSystemViewModel_Tests.cs
@@ -30,6 +30,23 @@ namespace PictureLibraryViewModel.Tests
             Assert.True(fileSystemVM.Drives.Count == 5);
         }
 
+        [Fact]
+        public void CurrentDirectoryContentGet_ShouldReturnListOfDirectoriesAndImageFiles_WhenCurrentDirectoryPathDoesNotEqualMyComputer()
+        {
+            string currDirectoryPath = "ff";
+            var mock = new Mock<IFileSystemService>();
+            mock.Setup(x => x.GetAllImageFiles(currDirectoryPath))
+                .Returns(GetImageFilesSample());
+            mock.Setup(x => x.GetAllDirectories(currDirectoryPath, SearchOption.TopDirectoryOnly))
+                .Returns(GetDirectoriesSample());
+
+            var fileSystemVM = new FileSystemViewModel(mock.Object);
+            Task.WaitAll(fileSystemVM.Initialize());
+            fileSystemVM.CurrentDirectoryPath = currDirectoryPath;
+
+            Assert.True(fileSystemVM.CurrentDirectoryContent.Count == 10);
+        }
+
         private ObservableCollection<Drive> GetDrivesSample()
         {
             var drives = new ObservableCollection<Drive>();
@@ -41,6 +58,32 @@ namespace PictureLibraryViewModel.Tests
             drives.Add(new Drive("Drive5", new FileSystemService()));
 
             return drives;
+        }
+
+        private List<ImageFile> GetImageFilesSample()
+        {
+            var imageFiles = new List<ImageFile>();
+            
+            imageFiles.Add(new ImageFile());
+            imageFiles.Add(new ImageFile());
+            imageFiles.Add(new ImageFile());
+            imageFiles.Add(new ImageFile());
+            imageFiles.Add(new ImageFile());
+
+            return imageFiles;
+        }
+
+        private ObservableCollection<Directory> GetDirectoriesSample()
+        {
+            var directories = new ObservableCollection<Directory>();
+
+            directories.Add(new Directory(null,""));
+            directories.Add(new Directory(null, ""));
+            directories.Add(new Directory(null, ""));
+            directories.Add(new Directory(null, ""));
+            directories.Add(new Directory(null, ""));
+
+            return directories;
         }
     }
 }
