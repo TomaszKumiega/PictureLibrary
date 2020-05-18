cd8ea02e6b2e3652d32c05593ab74af72b0535a2
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-05-18 11:16:42 +0200

    Add library class constructor

--- PictureLibraryModel/Model/Library.cs
+++ PictureLibraryModel/Model/Library.cs
@@ -6,8 +6,15 @@ namespace PictureLibraryModel.Model
 {
     public class Library
     {
-        public string FullPath { get; set; }
-        public string Name { get; set; }
-        public List<Album> Albums { get; set; }
+        public string FullPath { get; }
+        public string Name { get; }
+        public List<Album> Albums { get; }
+
+        public Library(string fullPath, string name)
+        {
+            FullPath = fullPath;
+            Name = name;
+            Albums = new List<Album>();
+        }
     }
 }
