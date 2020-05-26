9479f938a1eb821e0d66268e63eabc35e891be1e
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-05-26 11:45:53 +0200

    Add libraries viewmodel

---
+++ PictureLibraryViewModel/ViewModels/ILibrariesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModels
+{
+    public interface ILibrariesViewModel
+    {
+        /// <summary>
+        /// Contains all found libraries
+        /// </summary>
+        ObservableCollection<Library> Libraries { get; }
+        /// <summary>
+        /// Contains all library entities that should be displayed 
+        /// </summary>
+        ObservableCollection<ILibraryEntity> DisplayedEntities { get; }
+    }
+}
---
+++ PictureLibraryViewModel/ViewModels/LibrariesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModels
+{
+    public class LibrariesViewModel : ILibrariesViewModel
+    {
+        private ILibraryFileService _libraryFileService;
+        public ObservableCollection<Library> Libraries { get; }
+        public ObservableCollection<ILibraryEntity> DisplayedEntities { get; }
+
+        public LibrariesViewModel(ILibraryFileService libraryFileService)
+        {
+            _libraryFileService = libraryFileService;
+            Libraries = new ObservableCollection<Library>();
+            DisplayedEntities = new ObservableCollection<ILibraryEntity>();
+        }
+    }
+}
