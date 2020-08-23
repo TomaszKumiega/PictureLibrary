192ab230c774caeb3ccec785145b7839e158ed75
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-08-24 00:44:19 +0200

    Add library entities factory

---
+++ PictureLibraryModel/Model/ILibraryEntitiesFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace PictureLibraryModel.Model
+{
+    public interface ILibraryEntitiesFactory
+    {
+        Album GetAlbum(string name);
+        Album GetAlbum(string name, List<ImageFile> images);
+        ImageFile GetImageFile();
+        ImageFile GetImageFile(string path);
+        Library GetLibrary(string fullPath, string name);
+        Library GetLibrary(string fullPath, string name, List<Album> albums);
+    }
+}
---
+++ PictureLibraryModel/Model/LibraryEntitiesFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Model
+{
+    public class LibraryEntitiesFactory : ILibraryEntitiesFactory
+    {
+        public Library GetLibrary(string fullPath, string name)
+        {
+            return new Library(fullPath, name);
+        }
+
+        public Library GetLibrary(string fullPath, string name, List<Album> albums)
+        {
+            return new Library(fullPath, name, albums);
+        }
+
+        public ImageFile GetImageFile(string path)
+        {
+            return new ImageFile(path);
+        }
+
+        public ImageFile GetImageFile()
+        {
+            return new ImageFile();
+        }
+
+        public Album GetAlbum(string name)
+        {
+            return new Album(name);
+        }
+
+        public Album GetAlbum(string name, List<ImageFile> images)
+        {
+            return new Album(name, images);
+        }
+    }
+}
