14b05284bfe7ed3a07e761eb6c65182c44746ea4
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-05 10:35:28 +0100

    Add AuthenticateUserModel

---
+++ PictureLibraryModel/Model/RemoteServerUser/AuthenticateUserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PictureLibraryModel.Model.RemoteServerUser
+{
+    public class AuthenticateUserModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public AuthenticateUserModel()
+        {
+
+        }
+
+        public AuthenticateUserModel(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
