25243f504eaf5905e7de99d5417390ac32cfa5fe
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-05 08:30:55 +0100

    Add User model

---
+++ PictureLibraryModel/Model/RemoteServerUser/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Model.RemoteServerUser
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
