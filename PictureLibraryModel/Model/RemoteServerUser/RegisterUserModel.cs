09146a12c15e2fb0e5f2c6721c15dcd74e80f318
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-05 10:36:55 +0100

    Add RegisterUserModel

---
+++ PictureLibraryModel/Model/RemoteServerUser/RegisterUserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PictureLibraryModel.Model.RemoteServerUser
+{
+    public class RegisterUserModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public RegisterUserModel()
+        {
+
+        }
+
+        public RegisterUserModel(string username, string password, string email)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+        }
+    }
+}
+
