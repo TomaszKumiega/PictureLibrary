9d97e42e92fa579196af21528d0f478857c2f8fd
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-05 10:33:04 +0100

    Add Image model

---
+++ PictureLibraryModel/Model/Image.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Model
+{
+    public class Image
+    {
+        public ImageFile ImageFile { get; set; }
+        public byte[] ImageContent { get; set; }
+    }
+}
