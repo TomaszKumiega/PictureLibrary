bd21ed7427b5c7874e20d3cda63d4c398c20ff84
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-09 15:56:15 +0100

    Add FileExplorerViewModel

---
+++ PictureLibraryViewModel/ViewModel/FileExplorerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel
+{
+    public class FileExplorerViewModel : IFileExplorerViewModel
+    {
+        private FileSystemService _fileSystemService;
+        public ObservableCollection<IFileSystemEntity> DirectoryTree { get; private set; }
+        public ObservableCollection<IFileSystemEntity> CurrentDirectoryFiles { get; private set; }
+        public string CurrentDirectoryPath { get; set; }
+
+        public FileExplorerViewModel(FileSystemService fileSystemService)
+        {
+            _fileSystemService = fileSystemService;
+        }
+    }
+}
---
+++ PictureLibraryViewModel/ViewModel/IFileExplorerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PictureLibraryModel.Model;
+using System.Collections.ObjectModel;
+
+namespace PictureLibraryViewModel.ViewModel
+{
+    public interface IFileExplorerViewModel
+    {
+        ObservableCollection<IFileSystemEntity> CurrentDirectoryFiles { get; }
+        string CurrentDirectoryPath { get; set; }
+        ObservableCollection<IFileSystemEntity> DirectoryTree { get; }
+    }
+}
