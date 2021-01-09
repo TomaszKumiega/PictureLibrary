1252b128e13b60257580a22607857a948ff8e165
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-09 07:38:51 +0100

    Add FileSystemImageFile

---
+++ PictureLibraryModel/Model/FileSystemImageFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Model
+{
+    public class FileSystemImageFile : ImageFile, IFileSystemEntity
+    {
+        public string IconSource { get; set; }
+
+        public FileSystemImageFile()
+        {
+            IconSource = FullPath;
+        }
+    }
+}
