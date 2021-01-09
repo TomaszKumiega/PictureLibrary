9ab6fba08e77a4002fc19df98dc4b4cbf872c12d
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-09 14:57:19 +0100

    Add FileSystemService for Windows

---
+++ PictureLibraryModel/Services/WindowsFileSystemService.cs
@@ -0,0 +1,26 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace PictureLibraryModel.Services
+{
+    public class WindowsFileSystemService : FileSystemService
+    {
+        public override IEnumerable<IFileSystemEntity> GetRootDirectories()
+        {
+            var rootDirectories = new List<IFileSystemEntity>();
+
+            foreach(var driveinfo in DriveInfo.GetDrives())
+            {
+                if(System.IO.Directory.Exists(driveinfo.Name))
+                {
+                    rootDirectories.Add(new Drive(driveinfo.Name, driveinfo.Name, this));
+                }
+            }
+
+            return rootDirectories;
+        }
+    }
+}
