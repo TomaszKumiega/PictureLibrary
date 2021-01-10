b52fc41d31cbb5c8b06bf6c4e249cb49998ebe12
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-10 13:54:36 +0100

    Add CopyFileCommand unit tests

---
+++ PictureLibraryViewModel.Tests/CopyFileCommandTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using PictureLibraryViewModel.ViewModel;
+using PictureLibraryModel.Model;
+using PictureLibraryViewModel.Commands;
+
+namespace PictureLibraryViewModel.Tests
+{
+    public class CopyFileCommandTests
+    {
+        [Fact]
+        public void CanExecute_ShouldReturnTrue_WhenSelectedFileIsInitialized()
+        {
+            var imageFile = new Mock<ImageFile>();
+            var viewModel = new Mock<IExplorerViewModel>();
+            viewModel.Setup(x => x.SelectedFile)
+                .Returns(imageFile.Object);
+
+            var copyFileCommand = new CopyFileCommand(viewModel.Object);
+
+            Assert.True(copyFileCommand.CanExecute(new object()));
+        }
+
+        [Fact]
+        public void CanExecute_ShouldReturnFalse_WhenSelectedFileIsNull()
+        {
+            ImageFile file = null;
+            var viewModel = new Mock<IExplorerViewModel>();
+            viewModel.Setup(x => x.SelectedFile)
+                .Returns(file);
+
+            var copyFileCommand = new CopyFileCommand(viewModel.Object);
+
+            Assert.False(copyFileCommand.CanExecute(new object()));
+        }
+    }
+}
