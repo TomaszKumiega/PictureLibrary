086f98feeb26a05d829cb9f92a2ae620ea63c820
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-10 16:38:13 +0100

    Add PasteCommand

---
+++ PictureLibraryViewModel/Commands/PasteCommand.cs
@@ -0,0 +1,29 @@
+﻿using PictureLibraryViewModel.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PictureLibraryViewModel.Commands
+{
+    public class PasteCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private IExplorerViewModel _viewModel;
+
+        public PasteCommand(IExplorerViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+           return  _viewModel.CopiedFile != null;
+        }
+
+        public void Execute(object parameter)
+        {
+            _viewModel.PasteFile();
+        }
+    }
+}
