e0f47f2b6bfd5a99a903dbf288c3678743150102
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-10 16:37:50 +0100

    Add PasteCommandTests

---
+++ PictureLibraryViewModel.Tests/CommandTests/PasteCommandTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using PictureLibraryModel.Model;
+using PictureLibraryViewModel.Commands;
+using PictureLibraryViewModel.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PictureLibraryViewModel.Tests.CommandTests
+{
+    public class PasteCommandTests
+    {
+        [Fact]
+        public void CanExecute_ShouldReturnTrue_WhenCopiedFileIsInitialized()
+        {
+            var imageFileMock = new Mock<ImageFile>();
+
+            var viewModelMock = new Mock<IExplorerViewModel>();
+            viewModelMock.Setup(x => x.CopiedFile)
+                .Returns(imageFileMock.Object);
+
+            var pasteCommand = new PasteCommand(viewModelMock.Object);
+
+            Assert.True(pasteCommand.CanExecute(new object()));
+        }
+
+        [Fact]
+        public void CanExecute_ShouldReturnFalse_WhenCopiedFileIsNull()
+        {
+            ImageFile imageFile = null;
+
+            var viewModelMock = new Mock<IExplorerViewModel>();
+            viewModelMock.Setup(x => x.CopiedFile)
+                .Returns(imageFile);
+
+            var pasteCommand = new PasteCommand(viewModelMock.Object);
+
+            Assert.False(pasteCommand.CanExecute(new object()));
+        }
+    }
+}
