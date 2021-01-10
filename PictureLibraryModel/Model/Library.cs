ce0e05f6c8d190826f8f86e27dc1a2a4f51883e9
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-10 12:11:10 +0100

    Remove unnecessary constructors

--- PictureLibraryModel/Model/Library.cs
+++ PictureLibraryModel/Model/Library.cs
@@ -12,32 +12,6 @@ namespace PictureLibraryModel.Model
         public List<Tag> Tags { get; set; }
         public List<ImageFile> Images { get; set; }
         public List<Guid> Owners { get; set; }
-        public Origin Origin { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public Library()
-        {
-            Tags = new List<Tag>();
-            Images = new List<ImageFile>();
-        }
-
-        public Library(string fullPath, string name, string decription, List<Guid> owners)
-        {
-            FullPath = fullPath;
-            Name = name;
-            Description = decription;
-            Owners = owners;
-            Tags = new List<Tag>();
-            Images = new List<ImageFile>();
-        }
-
-        public Library(string fullPath, string name, string description, List<Guid> owners, List<Tag> tags, List<ImageFile> images)
-        {
-            FullPath = fullPath;
-            Name = name;
-            Description = description;
-            Owners = owners;
-            Tags = tags;
-            Images = images;
-        }
+        public Origin Origin { get; set; }
     }
 }
