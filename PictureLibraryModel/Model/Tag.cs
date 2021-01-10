cc63735bf1f9064edd8cb45c8339cb3bcba65cab
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-10 12:11:50 +0100

    Remove unnecessary constructors

--- PictureLibraryModel/Model/Tag.cs
+++ PictureLibraryModel/Model/Tag.cs
@@ -8,16 +8,5 @@ namespace PictureLibraryModel.Model
     { 
         public string Name { get; set; }
         public string Description { get; set; }
-
-        public Tag()
-        {
-
-        }
-
-        public Tag(string name, string description)
-        {
-            Name = name;
-            Description = description;
-        }
     }
 }
