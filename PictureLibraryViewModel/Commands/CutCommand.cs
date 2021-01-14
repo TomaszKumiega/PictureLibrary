c40aeffa393e2548e2f4454294b96cbc2c8d2c10
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-14 10:18:35 +0100

    Add CutCommand

---
+++ PictureLibraryViewModel/Commands/CutCommand.cs
@@ -0,0 +1,29 @@
+﻿using PictureLibraryViewModel.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PictureLibraryViewModel.Commands
+{
+    public class CutCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private IExplorerViewModel _viewModel;
+
+        public CutCommand(IExplorerViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
