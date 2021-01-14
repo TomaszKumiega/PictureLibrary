592ad461d341f8ca0d93c0d7e6d465e2b06d074b
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-14 11:38:39 +0100

    Add MobileDeviceClipboardService

---
+++ PictureLibraryModel/Services/Clipboard/MoblieDeviceClipboardService.cs
@@ -0,0 +1,19 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Services.Clipboard
+{
+    public class MoblieDeviceClipboardService : IClipboardService
+    {
+        public string SystemClipboard
+        {
+            get => Xamarin.Essentials.Clipboard.GetTextAsync().Result;
+            set => Xamarin.Essentials.Clipboard.SetTextAsync(value).Wait();
+        }
+
+        public IExplorableElement CopiedElement { get; set; }
+        public IExplorableElement CutElement { get; set; }
+    }
+}
