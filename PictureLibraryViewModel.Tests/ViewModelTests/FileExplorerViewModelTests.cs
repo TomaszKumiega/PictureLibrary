330ff3550b087330539ff76dc6f48a5aa263b0a5
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-14 09:31:42 +0100

    Add "PastFile method should call CopyMethod when CopiedFile is initialized" unit test

--- PictureLibraryViewModel.Tests/ViewModelTests/FileExplorerViewModelTests.cs
+++ PictureLibraryViewModel.Tests/ViewModelTests/FileExplorerViewModelTests.cs
@@ -24,5 +24,32 @@ namespace PictureLibraryViewModel.Tests
 
             Assert.True(viewModel.CopiedFile.Equals(imageFile));
         }
+
+        [Fact]
+        public void PastFile_ShouldCallCopyMethod_WhenCopiedFileIsInitialized()
+        {
+            var windowsFileSystemMock = new Mock<WindowsFileSystemService>();
+            var commandFactoryMock = new Mock<ICommandFactory>();
+            var imageFile =
+                new ImageFile()
+                {
+                    Name = "testFile.jpg",
+                    FullPath = "Tests\\Directory\\testFile.jpg"
+                };
+            var destination = "Tests\\Directory2\\";
+            bool copyMethodWasCalled = false;
+
+            windowsFileSystemMock.Setup(x => x.Copy(imageFile, destination))
+                .Callback(() => { copyMethodWasCalled = true; });
+
+            var viewModel = new FileExplorerViewModel(windowsFileSystemMock.Object, commandFactoryMock.Object);
+
+            viewModel.CopiedFile = imageFile;
+            viewModel.CurrentDirectoryPath = destination;
+
+            viewModel.PasteFile();
+
+            Assert.True(copyMethodWasCalled);
+        }
     }
 }
