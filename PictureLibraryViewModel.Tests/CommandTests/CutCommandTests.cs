9b625c842051500226f446ae16a11367b060f8ee
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-14 10:23:04 +0100

    Add "CanExecute method should return true when SelectedFile is initialized" unit test

--- PictureLibraryViewModel.Tests/CommandTests/CutCommandTests.cs
+++ PictureLibraryViewModel.Tests/CommandTests/CutCommandTests.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using Moq;
+using PictureLibraryModel.Model;
+using PictureLibraryViewModel.ViewModel;
+using PictureLibraryViewModel.Commands;
 
 namespace PictureLibraryViewModel.Tests.CommandTests
 {
     public class CutCommandTests
     {
+        [Fact]
+        public void CanExecute_ShouldReturnTrue_WhenSelectedFileIsInitialized()
+        {
+            var imageFileMock = new Mock<ImageFile>();
+
+            var viewModelMock = new Mock<IExplorerViewModel>();
+            viewModelMock.Setup(x => x.SelectedFile)
+                .Returns(imageFileMock.Object);
+
+            var cutCommand = new CutCommand(viewModelMock.Object);
+
+            Assert.True(cutCommand.CanExecute(new object()));
+        }
     }
 }
