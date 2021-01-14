4cf2185b7c4b932b342de26c8385789e408087cd
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-14 11:27:31 +0100

    Add ClipboardService

---
+++ PictureLibraryModel/Services/Clipboard/ClipboardService.cs
@@ -0,0 +1,18 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Services.Clipboard
+{
+    public class ClipboardService : IClipboardService
+    {
+        public string SystemClipboard
+        {
+            get => TextCopy.ClipboardService.GetText(); 
+            set => TextCopy.ClipboardService.SetText(value);
+        }
+        public IExplorableElement CopiedElement { get; set; }
+        public IExplorableElement CutElement { get; set; }
+    }
+}
---
+++ PictureLibraryModel/Services/Clipboard/IClipboardService.cs
@@ -0,0 +1,15 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Services.Clipboard
+{
+    public interface IClipboardService
+    {
+        string SystemClipboard { get; set; }
+
+        IExplorableElement CopiedElement { get; set; }
+        IExplorableElement CutElement { get; set; }
+    }
+}
