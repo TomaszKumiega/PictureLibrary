4a0b21b8ef76162588e8bfc7961f937457b9e664
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-15 11:50:05 +0100

    Implement is file an image method

--- PictureLibraryModel/Model/ImageFile.cs
+++ PictureLibraryModel/Model/ImageFile.cs
@@ -1,10 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace PictureLibraryModel.Model
 {
+    public enum ImageExtensions
+    {
+        JPG,
+        BMP,
+        GIF,
+        PNG,
+        TIFF,
+        NONE
+    }
+
+    public class ImageExtension
+    {
+        public static ImageExtensions GetExtension(byte[] bytes)
+        {
+            var bmp = Encoding.ASCII.GetBytes("BM");     // BMP
+            var gif = Encoding.ASCII.GetBytes("GIF");    // GIF
+            var png = new byte[] { 137, 80, 78, 71 };    // PNG
+            var tiff = new byte[] { 73, 73, 42 };         // TIFF
+            var tiff2 = new byte[] { 77, 77, 42 };         // TIFF
+            var jpeg = new byte[] { 255, 216, 255, 224 }; // jpeg
+            var jpeg2 = new byte[] { 255, 216, 255, 225 }; // jpeg2
+
+            if (bmp.SequenceEqual(bytes.Take(bmp.Length)))
+                return ImageExtensions.BMP;
+
+            if (gif.SequenceEqual(bytes.Take(gif.Length)))
+                return ImageExtensions.GIF;
+
+            if (png.SequenceEqual(bytes.Take(png.Length)))
+                return ImageExtensions.PNG;
+
+            if (tiff.SequenceEqual(bytes.Take(tiff.Length)))
+                return ImageExtensions.TIFF;
+
+            if (tiff2.SequenceEqual(bytes.Take(tiff2.Length)))
+                return ImageExtensions.TIFF;
+
+            if (jpeg.SequenceEqual(bytes.Take(jpeg.Length)))
+                return ImageExtensions.JPG;
+
+            if (jpeg2.SequenceEqual(bytes.Take(jpeg2.Length)))
+                return ImageExtensions.JPG;
+
+            return ImageExtensions.NONE;
+        }
+    }
+
     public class ImageFile : IExplorableElement
     {
         public string Name { get; set; }
@@ -24,5 +73,10 @@ namespace PictureLibraryModel.Model
             IconSource = FullPath;
         }
 
+        public static bool IsFileAnImage(string path)
+        {
+            var fileBytes = File.ReadAllBytes(path);
+            return ImageExtension.GetExtension(fileBytes) != ImageExtensions.NONE;
+        }
     }
 }
