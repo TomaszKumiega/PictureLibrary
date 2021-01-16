bf86c34ff159fd7fe584719a3e670563212ee252
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-16 13:21:54 +0100

    Add "BuildExtension method should initialize Extension when FileInfo is initialized" unit test

--- PictureLibraryModel.Tests/ModelTests/LocalFileSystemImageFileBuilderTests.cs
+++ PictureLibraryModel.Tests/ModelTests/LocalFileSystemImageFileBuilderTests.cs
@@ -1,4 +1,5 @@
 ﻿using Moq;
+using PictureLibraryModel.Model;
 using PictureLibraryModel.Model.Builders;
 using System;
 using System.Collections.Generic;
@@ -29,8 +30,24 @@ namespace PictureLibraryModel.Tests.ModelTests
 
             builder.BuildCreationTime();
 
-            Assert.True(DateTime.Compare(dateTime, builder.ImageFile.CreationTime) == 0);
-            
+            Assert.True(DateTime.Compare(dateTime, builder.ImageFile.CreationTime) == 0);   
+        }
+
+        [Fact]
+        public void BuildExtension_ShouldInitializeExtension_WhenFileInfoIsInitialized()
+        {
+            var extensionString = ".jpg";
+            var extension = ImageExtensionHelper.GetExtension(extensionString);
+            var fileInfoMock = new Mock<IFileInfo>();
+
+            fileInfoMock.Setup(x => x.Extension)
+                .Returns(extensionString);
+
+            var builder = new LocalFileSystemImageFileBuilder(fileInfoMock.Object);
+
+            builder.BuildExtension();
+
+            Assert.True(extension == builder.ImageFile.Extension);
         }
     }
 }
