9c779d26a43ca84bceaf7cd7872fb4e1fb1c3c5d
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-23 21:41:28 +0100

    Change image loading to async and scale image to save memory

---
+++ PictureLibraryWPF/Helpers/ImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace PictureLibraryWPF.Helpers
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.UriSource = new Uri(value.ToString());
+            bitmap.DecodePixelWidth = 50;
+            bitmap.EndInit();
+
+            return bitmap;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
