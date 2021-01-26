c83469c70cdb757f6609c0351b20ba80b95e894f
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-26 03:01:33 +0100

    Add FileService class

---
+++ PictureLibraryModel/Services/FileSystemServices/FileService.cs
@@ -0,0 +1,43 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PictureLibraryModel.Services.FileSystemServices
+{
+    public class FileService : IFileService
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public void Copy(string sourcePath, string destinationPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileSystemInfo GetInfo(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(string sourcePath, string destinationPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileStream OpenFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rename(string path, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
