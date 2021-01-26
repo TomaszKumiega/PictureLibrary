e4ff71d84c9783437de4624fea2ddbbac1d4f71c
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-26 03:50:08 +0100

    Add DirectoryService class

---
+++ PictureLibraryModel/Services/FileSystemServices/DirectoryService.cs
@@ -0,0 +1,51 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PictureLibraryModel.Services.FileSystemServices
+{
+    public class DirectoryService : IDirectoryService
+    {
+        public void Copy(string sourcePath, string destinationPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IExplorableElement> GetDirectoryContent(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileSystemInfo GetInfo(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.Directory> GetRootDirectories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Folder> GetSubFolders(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(string sourcePath, string destinationPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rename(string path, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
