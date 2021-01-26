18e1708a4e4d032610aff2c790769f6b4bbde645
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-26 03:00:09 +0100

    Add IDirectoryService interface

---
+++ PictureLibraryModel/Services/FileSystemServices/IDirectoryService.cs
@@ -0,0 +1,14 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Services.FileSystemServices
+{
+    public interface IDirectoryService : IFileSystemService
+    {
+        IEnumerable<Folder> GetSubFolders(string path);
+        IEnumerable<Directory> GetRootDirectories();
+        IEnumerable<IExplorableElement> GetDirectoryContent(string path);
+    }
+}
