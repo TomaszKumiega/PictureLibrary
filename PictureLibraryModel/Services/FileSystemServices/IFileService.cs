5bf85d019e86ae89eaabb5c45cfe176c108e7a94
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-26 02:56:41 +0100

    Add IFileService interface

---
+++ PictureLibraryModel/Services/FileSystemServices/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PictureLibraryModel.Services.FileSystemServices
+{
+    public interface IFileService : IFileSystemService
+    {
+        FileStream OpenFile(string path);
+    }
+}
