d19d34ffe49fd8312a2fd193bc2d1acc8934c0c5
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-30 04:45:24 +0100

    Add setter to IconSource

--- PictureLibraryModel/Model/File.cs
+++ PictureLibraryModel/Model/File.cs
@@ -9,7 +9,7 @@ namespace PictureLibraryModel.Model
         public string Name { get; set; }
         public Origin Origin { get; set; }
         public string FullPath { get; set; }
-        public string IconSource { get; }
+        public string IconSource { get; set; }
         public DateTime CreationTime { get; set; }
         public DateTime LastAccessTime { get; set; }
         public DateTime LastWriteTime { get; set; }
