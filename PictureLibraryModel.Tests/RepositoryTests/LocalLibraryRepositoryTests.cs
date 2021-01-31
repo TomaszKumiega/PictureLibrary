d7a2f76e6ea0040e622a0e0a8627c2fc1aef5388
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-31 05:41:38 +0100

    Add "AddAsync method should write library to the stream when library is initialized" unit test

--- PictureLibraryModel.Tests/RepositoryTests/LocalLibraryRepositoryTests.cs
+++ PictureLibraryModel.Tests/RepositoryTests/LocalLibraryRepositoryTests.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Moq;
+using PictureLibraryModel.Model;
+using PictureLibraryModel.Repositories;
+using PictureLibraryModel.Services.FileSystemServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Xml.Linq;
+using Xunit;
 
 namespace PictureLibraryModel.Tests.RepositoryTests
 {
@@ -24,12 +30,61 @@ namespace PictureLibraryModel.Tests.RepositoryTests
         {
             try
             {
-                Directory.Delete(TestFolder, true);
+                System.IO.Directory.Delete(TestFolder, true);
             }
             catch
             {
                 return;
             }
+        }
+        #endregion
+
+        #region Helper methods
+        private Library GetLibrary()
+        {
+            var library =
+                new Library()
+                {
+                    FullPath = TestFolder + "library.plib",
+                    Name = "library",
+                    Description = "picture library",
+                    Tags = new List<Tag>(),
+                    Images = new List<ImageFile>(),
+                    Owners = new List<Guid>(),
+                    Origin = Origin.Local,
+                };
+
+            return library;
+        }
+        #endregion
+
+        #region AddAsync Tests
+        [Fact]
+        public async void AddAsync_ShouldWriteLibraryToTheStream_WhenLibraryIsInitialized()
+        {
+            var fileServiceMock = new Mock<IFileService>();
+            var memoryStream = new MemoryStream();
+            var library = GetLibrary();
+            
+            fileServiceMock.Setup(x => x.OpenFile(library.FullPath))
+                .Returns(memoryStream);
+
+            var repository = new LocalLibraryRepository(fileServiceMock.Object);
+            await repository.AddAsync(library);
+
+            // convert memory stream buffer to string
+            var buffer = memoryStream.GetBuffer();
+            var text = System.Text.Encoding.UTF8.GetString(buffer);
+            text = text.Replace('\0', ' ');
+            text = text.TrimEnd();
+
+            // read attributes from the string
+            var doc = XDocument.Parse(text);
+            var name = doc.Element("library").Attribute("name");
+            var description = doc.Element("library").Attribute("description");
+
+            Assert.True(name.Value == library.Name);
+            Assert.True(description.Value == library.Description);
         }
         #endregion
     }
