c1ef445c577ef9c82dea7e1950f67091d9168e01
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-31 04:25:03 +0100

    Add LocalLibraryRepositoryTests class

---
+++ PictureLibraryModel.Tests/RepositoryTests/LocalLibraryRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PictureLibraryModel.Tests.RepositoryTests
+{
+    public class LocalLibraryRepositoryTests
+    {
+        #region Setup
+        private string TestFolder = "Tests\\";
+
+        public LocalLibraryRepositoryTests()
+        {
+            CleanupFiles();
+        }
+
+        ~LocalLibraryRepositoryTests()
+        {
+            CleanupFiles();
+        }
+
+        private void CleanupFiles()
+        {
+            try
+            {
+                Directory.Delete(TestFolder, true);
+            }
+            catch
+            {
+                return;
+            }
+        }
+        #endregion
+    }
+}
