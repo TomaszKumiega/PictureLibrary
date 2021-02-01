d1c47358d22b59ca5bc4ba936e195b4c4968202c
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-01 08:49:33 +0100

    Add LibraryExplorerViewModel

---
+++ PictureLibraryViewModel/ViewModel/LibraryExplorerViewModel.cs
@@ -0,0 +1,59 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Repositories;
+using PictureLibraryModel.Services.Clipboard;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace PictureLibraryViewModel.ViewModel
+{
+    public class LibraryExplorerViewModel : ILibraryExplorerViewModel
+    { 
+        public ObservableCollection<IExplorableElement> SelectedElements { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IExplorableElement SelectedNode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ICommand CopyCommand => throw new NotImplementedException();
+
+        public ICommand PasteCommand => throw new NotImplementedException();
+
+        public ICommand CutCommand => throw new NotImplementedException();
+
+        public ICommand RenameCommand => throw new NotImplementedException();
+
+        public IClipboardService Clipboard => throw new NotImplementedException();
+
+        public ObservableCollection<IExplorableElement> ExplorableElementsTree => throw new NotImplementedException();
+
+        public ObservableCollection<IExplorableElement> CurrentlyShownElements => throw new NotImplementedException();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void CopySelectedElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CutSelectedElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PasteSelectedElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSelectedElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenameSelectedElements()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
