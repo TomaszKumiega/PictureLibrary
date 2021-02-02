d72005c75e65c67a020a0e62694bff1571e5e4eb
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-02 09:12:36 +0100

    Add IRepository interface

--- PictureLibraryModel/Repositories/ILibraryRepository.cs
+++ PictureLibraryModel/Repositories/ILibraryRepository.cs
@@ -6,16 +6,9 @@ using System.Threading.Tasks;
 
 namespace PictureLibraryModel.Repositories
 {
-    public interface ILibraryRepository
+    public interface ILibraryRepository : IRepository<Library>
     {
-        Task AddAsync(Library library);
-        Task AddRangeAsync(IEnumerable<Library> libraries);
-        Task<Library> FindAsync(Predicate<Library> predicate);
-        Task<IEnumerable<Library>> GetAllAsync();
         Task<Library> GetByPathAsync(string path);
         Task RemoveAsync(string path);
-        Task RemoveAsync(Library library);
-        Task RemoveRangeAsync(IEnumerable<Library> libraries);
-        Task UpdateAsync(Library library);
     } 
 }
