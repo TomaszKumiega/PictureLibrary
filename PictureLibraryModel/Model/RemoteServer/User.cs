f290be7b83139168707f73f83476db2b479870be
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-02 11:20:06 +0100

    Implement DatabaseEntity interface

--- PictureLibraryModel/Model/RemoteServer/User.cs
+++ PictureLibraryModel/Model/RemoteServer/User.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using PictureLibraryModel.Model.Database;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PictureLibraryModel.Model.RemoteServer
 {
-    public class User
+    public class User : IDatabaseEntity
     {
         public Guid Id { get; set; }
         public string Username { get; set; }
