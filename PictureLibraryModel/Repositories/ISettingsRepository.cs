80910c23e2b8a0f3dd15411e0bcfba340094cc51
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-02 11:46:49 +0100

    Add SettingsRepository

---
+++ PictureLibraryModel/Repositories/ISettingsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryModel.Repositories
+{
+    public interface ISettingsRepository : IRepository<Settings.Settings>
+    {
+        Task<Settings.Settings> GetByUserId(Guid id);
+    }
+}
---
+++ PictureLibraryModel/Repositories/SettingsRepository.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryModel.Repositories
+{
+    public class SettingsRepository : DatabaseRepository<Settings.Settings>, ISettingsRepository
+    {
+        public SettingsRepository() : base("Settings")
+        {
+
+        }
+
+        public async Task<Settings.Settings> GetByUserId(Guid id)
+        {
+            using(var conn = GetConnection())
+            {
+                var list = (await conn.QueryAsync<Settings.Settings>($"SELECT * FROM {_tableName} WHERE UserId=@Id", new { Id = id })).ToList();
+
+                if (list.Any()) return list[0];
+                else return null;
+            }
+        }
+    }
+}
