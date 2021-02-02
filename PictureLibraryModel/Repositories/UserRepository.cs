bc7262e290fa34884c3951d84de140b96638aa3a
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-02 11:20:33 +0100

    Add UserRepository

---
+++ PictureLibraryModel/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using PictureLibraryModel.Model.RemoteServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Repositories
+{
+    public class UserRepository : DatabaseRepository<User>
+    {
+        public UserRepository() : base("Users")
+        {
+
+        }
+    }
+}
