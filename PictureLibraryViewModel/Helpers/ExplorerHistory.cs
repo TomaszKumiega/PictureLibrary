3ad1b3a31e5e8313b60da29008ad73431b743b08
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-03 07:41:49 +0100

    Add ExplorerHistory class

---
+++ PictureLibraryViewModel/Helpers/ExplorerHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.Helpers
+{
+    public class ExplorerHistory : IExplorerHistory
+    {
+        public Stack<string> BackStack { get; }
+        public Stack<string> ForwardStack { get; }
+
+        public ExplorerHistory()
+        {
+            BackStack = new Stack<string>();
+            ForwardStack = new Stack<string>();
+        }
+    }
+}
---
+++ PictureLibraryViewModel/Helpers/IExplorerHistory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.Helpers
+{
+    public interface IExplorerHistory
+    {
+        Stack<string> BackStack { get; }
+        Stack<string> ForwardStack { get; }
+    }
+}
