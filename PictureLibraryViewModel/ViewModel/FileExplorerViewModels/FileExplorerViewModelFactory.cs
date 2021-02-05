845f2d78af46a6c11807ee2fc8d73f7f56e0dd2f
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-05 19:29:11 +0100

    Add FileExplorerViewModel factory

---
+++ PictureLibraryViewModel/ViewModel/FileExplorerViewModels/FileExplorerViewModelFactory.cs
@@ -0,0 +1,33 @@
+﻿using PictureLibraryModel.Services.Clipboard;
+using PictureLibraryModel.Services.FileSystemServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.FileExplorerViewModels
+{
+    public class FileExplorerViewModelFactory : IFileExplorerViewModelFactory
+    {
+        private IFileExplorerViewModel _commonVM;
+
+        public FileExplorerViewModelFactory(IFileExplorerViewModel commonVM)
+        {
+            _commonVM = commonVM;
+        }
+
+        public IFileExplorerToolboxViewModel GetFileToolboxViewModel(IClipboardService clipboard)
+        {
+            return new FileExplorerToolboxViewModel(_commonVM, new FileService(), new DirectoryService(), clipboard);
+        }
+
+        public IFileTreeViewModel GetFileTreeViewModel()
+        {
+            return new FileTreeViewModel(_commonVM);
+        }
+
+        public IFilesViewViewModel GetFilesViewViewModel()
+        {
+            return new FilesViewViewModel(_commonVM);
+        }
+    }
+}
---
+++ PictureLibraryViewModel/ViewModel/FileExplorerViewModels/IFileExplorerViewModelFactory.cs
@@ -0,0 +1,14 @@
+﻿using PictureLibraryModel.Services.Clipboard;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.FileExplorerViewModels
+{
+    public interface IFileExplorerViewModelFactory
+    {
+        IFilesViewViewModel GetFilesViewViewModel();
+        IFileTreeViewModel GetFileTreeViewModel();
+        IFileExplorerToolboxViewModel GetFileToolboxViewModel(IClipboardService clipboard);
+    }
+}
