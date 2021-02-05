ca15a7df94b719ac8b8ed50451f4a14eabee5483
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-05 19:21:18 +0100

    Add FilesViewViewModel

---
+++ PictureLibraryViewModel/ViewModel/FileExplorerViewModels/FilesViewViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.FileExplorerViewModels
+{
+    public class FilesViewViewModel : IFilesViewViewModel
+    {
+        public IFileExplorerViewModel CommonViewModel { get; }
+
+        public FilesViewViewModel(IFileExplorerViewModel viewModel)
+        {
+            CommonViewModel = viewModel;
+        }
+    }
+}
---
+++ PictureLibraryViewModel/ViewModel/FileExplorerViewModels/IFilesViewViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.FileExplorerViewModels
+{
+    public interface IFilesViewViewModel
+    {
+        IFileExplorerViewModel CommonViewModel { get; }
+    }
+}
