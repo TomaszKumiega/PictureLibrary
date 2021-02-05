a82d404ff34e8a205757863039cbcdb4153b56cc
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-05 15:11:05 +0100

    Add FileExplorerViewModel

---
+++ PictureLibraryViewModel/ViewModel/FileExplorerViewModels/FileExplorerViewModel.cs
@@ -0,0 +1,80 @@
+﻿using NLog;
+using PictureLibraryModel.Model;
+using PictureLibraryModel.Services.FileSystemServices;
+using PictureLibraryViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.FileExplorerViewModels
+{
+    public class FileExplorerViewModel : IFileExplorerViewModel
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private string _currentDirectoryPath;
+        private IDirectoryService _directoryService;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<IExplorableElement> CurrentlyShownElements { get; }
+        public ObservableCollection<IExplorableElement> SelectedElements { get; set; }
+        public IExplorerHistory ExplorerHistory { get; }
+
+        public string CurrentDirectoryPath 
+        {
+            get => _currentDirectoryPath;
+            set
+            {
+                ExplorerHistory.BackStack.Push(_currentDirectoryPath);
+                ExplorerHistory.ForwardStack.Clear();
+
+                _currentDirectoryPath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentDirectoryPath"));
+            }
+        }
+
+        public FileExplorerViewModel(IDirectoryService directoryService, IExplorerHistory explorerHistory)
+        {
+            _directoryService = directoryService;
+            ExplorerHistory = explorerHistory;
+            _currentDirectoryPath = "\\";
+
+            LoadCurrentDirectoryContent();
+        }
+
+        public void LoadCurrentDirectoryContent()
+        {
+            if (CurrentlyShownElements == null) return;
+
+            CurrentlyShownElements.Clear();
+
+            IEnumerable<IExplorableElement> content = new List<IExplorableElement>();
+
+            try
+            {
+                if (CurrentDirectoryPath == "\\")
+                {
+                    content = _directoryService.GetRootDirectories();
+                }
+                else
+                {
+                    content = _directoryService.GetDirectoryContent(CurrentDirectoryPath);
+                }
+            }
+            catch(Exception e)
+            {
+                _logger.Error(e, e.Message);
+                throw new Exception("Application failed loading the contents of: " + CurrentDirectoryPath + " directory.");
+            }
+
+            foreach (var t in content)
+            {
+                CurrentlyShownElements.Add(t);
+            }
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentlyShownElements"));
+        }
+    }
+}
---
+++ PictureLibraryViewModel/ViewModel/FileExplorerViewModels/IFileExplorerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.FileExplorerViewModels
+{
+    public interface IFileExplorerViewModel
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+
+        string CurrentDirectoryPath { get; set; }
+        ObservableCollection<IExplorableElement> CurrentlyShownElements { get; }
+        ObservableCollection<IExplorableElement> SelectedElements { get; set; }
+        IExplorerHistory ExplorerHistory { get; }
+    }
+}
