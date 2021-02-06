d4fb9e15d0959de6895d61987227de80221c2dcc
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-06 13:51:56 +0100

    Initialize directory tree

--- PictureLibraryViewModel/ViewModel/FileExplorerViewModels/FileTreeViewModel.cs
+++ PictureLibraryViewModel/ViewModel/FileExplorerViewModels/FileTreeViewModel.cs
@@ -1,17 +1,21 @@
-﻿using PictureLibraryModel.Model;
+﻿using NLog;
+using PictureLibraryModel.Model;
+using PictureLibraryModel.Services.FileSystemServices;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PictureLibraryViewModel.ViewModel.FileExplorerViewModels
 {
     public class FileTreeViewModel : IFileTreeViewModel
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         private IExplorableElement _selectedNode;
 
         public IExplorerViewModel CommonViewModel { get; }
-        public ObservableCollection<IExplorableElement> ExplorableElementsTree { get; }
+        public ObservableCollection<IExplorableElement> ExplorableElementsTree { get; private set; }
         public IExplorableElement SelectedNode 
         { 
             get => _selectedNode; 
@@ -24,8 +28,33 @@ namespace PictureLibraryViewModel.ViewModel.FileExplorerViewModels
 
         public FileTreeViewModel(IFileExplorerViewModel viewModel)
         {
-            ExplorableElementsTree = new ObservableCollection<IExplorableElement>();
             CommonViewModel = viewModel;
+            InitializeDirectoryTree();
+        }
+
+        private void InitializeDirectoryTree()
+        {
+            ExplorableElementsTree = new ObservableCollection<IExplorableElement>();
+
+            IEnumerable<Directory> rootDirectories = new List<Directory>();
+
+            try
+            {
+                rootDirectories = CommonViewModel.DirectoryService.GetRootDirectories();
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, e.Message);
+                throw new Exception("Application failed loading the directory tree");
+            }
+
+            foreach (var t in rootDirectories)
+            {
+                Task.Run(() => t.LoadSubDirectoriesAsync()).Wait();
+                ExplorableElementsTree.Add(t);
+            }
+
+            CommonViewModel.CurrentlyOpenedPath = "\\";
         }
     }
 }
