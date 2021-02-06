19a528d977cfe65c3eb3d9df5e075868191f6609
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-06 12:19:13 +0100

    Add IExplorerToolboxViewModel interface

--- PictureLibraryViewModel/ViewModel/FileExplorerViewModels/IFileExplorerToolboxViewModel.cs
+++ PictureLibraryViewModel/ViewModel/FileExplorerViewModels/IFileExplorerToolboxViewModel.cs
@@ -6,30 +6,12 @@ using System.Windows.Input;
 
 namespace PictureLibraryViewModel.ViewModel.FileExplorerViewModels
 {
-    public interface IFileExplorerToolboxViewModel
+    public interface IFileExplorerToolboxViewModel : IExplorerToolboxViewModel
     {
-        IFileExplorerViewModel CommonViewModel { get; }
-        IClipboardService Clipboard { get; }
-        ICommand CopyCommand { get; }
-        ICommand PasteCommand { get; }
-        ICommand CutCommand { get; }
-        ICommand CopyPathCommand { get; }
-        ICommand RemoveCommand { get; }
-        ICommand RenameCommand { get; }
         ICommand CreateFolderCommand { get; }
-        ICommand BackCommand { get; }
-        ICommand ForwardCommand { get; }
         ICommand GoToParentDirectoryCommand { get; }
-        ICommand RefreshCommand { get; }
-        string SearchText { get; set; }
 
-        void Copy();
-        void Paste();
-        void CopyPath();
-        void Remove();
-        void Rename();
         void CreateDirectory();
         void GoToParentDirectory();
-        void Refresh();
     }
 }
---
+++ PictureLibraryViewModel/ViewModel/IExplorerToolboxViewModel.cs
@@ -0,0 +1,31 @@
+﻿using PictureLibraryModel.Services.Clipboard;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PictureLibraryViewModel.ViewModel
+{
+    public interface IExplorerToolboxViewModel
+    {
+        IExplorerViewModel CommonViewModel { get; }
+        IClipboardService Clipboard { get; }
+        ICommand CopyCommand { get; }
+        ICommand PasteCommand { get; }
+        ICommand CutCommand { get; }
+        ICommand CopyPathCommand { get; }
+        ICommand RemoveCommand { get; }
+        ICommand RenameCommand { get; }
+        ICommand RefreshCommand { get; }
+        ICommand BackCommand { get; }
+        ICommand ForwardCommand { get; }
+        string SearchText { get; set; }
+
+        void Copy();
+        void Paste();
+        void CopyPath();
+        void Remove();
+        void Rename();
+        void Refresh();
+    }
+}
