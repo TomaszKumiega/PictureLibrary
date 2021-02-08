56bc281618dc8b430aac70ff8ed44d785adc3a08
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-08 16:28:10 +0100

    Add FileExplorerViewModelTests class

---
+++ PictureLibraryViewModel.Tests/ViewModelTests/FileExplorerViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using PictureLibraryModel.Services.FileSystemServices;
+using PictureLibraryViewModel.Helpers;
+using PictureLibraryViewModel.ViewModel.FileExplorerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PictureLibraryViewModel.Tests.ViewModelTests
+{
+    public class FileExplorerViewModelTests
+    {
+
+    }
+}
