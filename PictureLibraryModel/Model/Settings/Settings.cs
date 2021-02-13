ec3d74455705c20f90223e82468b3ac63ed16a0d
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-13 01:32:02 +0100

    Change settings model

--- PictureLibraryModel/Model/Settings/Settings.cs
+++ PictureLibraryModel/Model/Settings/Settings.cs
@@ -1,14 +1,11 @@
-﻿using PictureLibraryModel.Model.Database;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PictureLibraryModel.Model.Settings
 {
-    public class Settings : IDatabaseEntity
+    public class Settings 
     {
-        public Guid Id { get; set; }
-        public Guid UserId { get; set; }
         public bool LightMode { get; set; }
         public string AccentColor { get; set; }
         public string Language { get; set; }
