c2a87b547bedfafbae21755b715f538b2125f8cf
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-15 05:19:29 +0100

    Implement Back and Forward methods

--- PictureLibraryViewModel/ViewModel/LibraryExplorerViewModels/LibraryExplorerViewModel.cs
+++ PictureLibraryViewModel/ViewModel/LibraryExplorerViewModels/LibraryExplorerViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +14,9 @@ namespace PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels
     public class LibraryExplorerViewModel : ILibraryExplorerViewModel
     {
         private IRepository<Library> _libraryRepository;
+        private IExplorableElement _currentlyOpenedElement;
 
-        public ObservableCollection<IExplorableElement> CurrentlyShownElements { get; }
+        public ObservableCollection<IExplorableElement> CurrentlyShownElements { get; set; }
         public ObservableCollection<IExplorableElement> SelectedElements { get; set; }
         public string InfoText { get; set; }
         public bool IsProcessing { get; set; }
@@ -26,22 +28,32 @@ namespace PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels
 
         public LibraryExplorerViewModel(IRepository<Library> libraryRepository)
         {
+            CurrentlyShownElements = new ObservableCollection<IExplorableElement>();
+            SelectedElements = new ObservableCollection<IExplorableElement>();
             _libraryRepository = libraryRepository;
         }
 
-        public Task LoadCurrentlyShownElements()
+        public async Task LoadCurrentlyShownElements()
         {
             throw new NotImplementedException();
         }
 
         public void Back()
         {
-            throw new NotImplementedException();
+            if (ExplorerHistory.BackStack.Count == 0) return;
+
+            ExplorerHistory.ForwardStack.Push(_currentlyOpenedElement);
+            _currentlyOpenedElement = ExplorerHistory.BackStack.Pop();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentlyOpenedElement"));
         }
 
         public void Forward()
         {
-            throw new NotImplementedException();
+            if (ExplorerHistory.ForwardStack.Count == 0) return;
+
+            ExplorerHistory.BackStack.Push(_currentlyOpenedElement);
+            _currentlyOpenedElement = ExplorerHistory.ForwardStack.Pop();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentlyOpenedElement"));
         }
     }
 }
