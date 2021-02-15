2bf7a7e0c7b3692f285aaf218a5e2fe8d1028162
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-15 23:51:51 +0100

    Add LibraryViewViewModel

---
+++ PictureLibraryViewModel/ViewModel/LibraryExplorerViewModels/LibraryViewViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels
+{
+    public class LibraryViewViewModel : IExplorableElementsViewViewModel
+    {
+        public IExplorerViewModel CommonViewModel { get; }
+
+        public LibraryViewViewModel(ILibraryExplorerViewModel commonVM)
+        {
+            CommonViewModel = commonVM;
+        }
+    }
+}
