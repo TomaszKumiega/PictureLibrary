7c7b7846f6d2a031fcb9effe04b5e0162fcff0fa
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-15 05:04:12 +0100

    Add LibraryExplorerViewModel

---
+++ PictureLibraryViewModel/ViewModel/LibraryExplorerViewModels/ILibraryExplorerViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels
+{
+    public interface ILibraryExplorerViewModel : IExplorerViewModel
+    {
+    }
+}
---
+++ PictureLibraryViewModel/ViewModel/LibraryExplorerViewModels/LibraryExplorerViewModel.cs
@@ -0,0 +1,47 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Repositories;
+using PictureLibraryViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels
+{
+    public class LibraryExplorerViewModel : ILibraryExplorerViewModel
+    {
+        private IRepository<Library> _libraryRepository;
+
+        public ObservableCollection<IExplorableElement> CurrentlyShownElements { get; }
+        public ObservableCollection<IExplorableElement> SelectedElements { get; set; }
+        public string InfoText { get; set; }
+        public bool IsProcessing { get; set; }
+        public IExplorableElement CurrentlyOpenedElement { get; set; }
+
+        public IExplorerHistory ExplorerHistory { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LibraryExplorerViewModel(IRepository<Library> libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        public Task LoadCurrentlyShownElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Back()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Forward()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
