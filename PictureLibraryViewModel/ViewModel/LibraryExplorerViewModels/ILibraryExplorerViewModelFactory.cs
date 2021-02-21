4e99cb8a413ccd0c264745d22cca226c5f615ff9
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-21 11:26:15 +0100

    Add LibraryExplorerViewModelFactory

---
+++ PictureLibraryViewModel/ViewModel/LibraryExplorerViewModels/ILibraryExplorerViewModelFactory.cs
@@ -0,0 +1,14 @@
+﻿using PictureLibraryModel.Services.Clipboard;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels
+{
+    public interface ILibraryExplorerViewModelFactory
+    {
+        ILibraryExplorerToolboxViewModel GetLibraryExplorerToolboxViewModel(IClipboardService clipboard);
+        IExplorableElementsTreeViewModel GetLibraryTreeViewModel();
+        IExplorableElementsViewViewModel GetLibraryViewViewModel();
+    }
+}
---
+++ PictureLibraryViewModel/ViewModel/LibraryExplorerViewModels/LibraryExplorerViewModelFactory.cs
@@ -0,0 +1,36 @@
+﻿using PictureLibraryModel.Repositories.LibraryRepositories;
+using PictureLibraryModel.Services.Clipboard;
+using PictureLibraryModel.Services.ConnectedServicesInfoProvider;
+using PictureLibraryViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels
+{
+    public class LibraryExplorerViewModelFactory : ILibraryExplorerViewModelFactory
+    {
+        private ILibraryExplorerViewModel CommonViewModel { get; }
+        private IConnectedServicesInfoProviderService ConnectedServices { get; }
+        public LibraryExplorerViewModelFactory(ILibraryExplorerViewModel commonVM, IConnectedServicesInfoProviderService connectedServices)
+        {
+            CommonViewModel = commonVM;
+            ConnectedServices = connectedServices;
+        }
+
+        public ILibraryExplorerToolboxViewModel GetLibraryExplorerToolboxViewModel(IClipboardService clipboard)
+        {
+            return new LibraryExplorerToolboxViewModel(CommonViewModel, new CommandFactory(), clipboard);
+        }
+
+        public IExplorableElementsTreeViewModel GetLibraryTreeViewModel()
+        {
+            return new LibraryTreeViewModel(new LibraryRepository(ConnectedServices, new LibraryRepositoriesFactory()));
+        }
+
+        public IExplorableElementsViewViewModel GetLibraryViewViewModel()
+        {
+            return new LibraryViewViewModel(CommonViewModel);
+        }
+    }
+}
