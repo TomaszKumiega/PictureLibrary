b6996e55cb5096eb03e0a1dceae3d24788c6afcb
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-21 10:09:17 +0100

    Add LibraryExplorerToolboxViewModel

---
+++ PictureLibraryViewModel/ViewModel/LibraryExplorerViewModels/LibraryExplorerToolboxViewModel.cs
@@ -0,0 +1,45 @@
+﻿using PictureLibraryModel.Services.Clipboard;
+using PictureLibraryViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels
+{
+    public class LibraryExplorerToolboxViewModel : ILibraryExplorerToolboxViewModel
+    {
+        public IExplorerViewModel CommonViewModel { get; }
+        public IClipboardService Clipboard { get; }
+        public ICommand RemoveCommand { get; }
+        public ICommand RenameCommand { get; }
+        public ICommand RefreshCommand { get; }
+        public string SearchText { get; set; }
+
+        public LibraryExplorerToolboxViewModel(ILibraryExplorerViewModel commonVM, ICommandFactory commandFactory, IClipboardService clipboard)
+        {
+            RemoveCommand = commandFactory.GetRemoveCommand(this);
+            RenameCommand = commandFactory.GetRenameCommand(this);
+            RefreshCommand = commandFactory.GetRefreshCommand(this);
+
+            Clipboard = clipboard;
+            CommonViewModel = commonVM;
+        }
+
+        public async Task Refresh()
+        {
+            await CommonViewModel.LoadCurrentlyShownElements();
+        }
+
+        public Task Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Rename()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
