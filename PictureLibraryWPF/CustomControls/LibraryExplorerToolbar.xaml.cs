5bd15114b00fed685339132388812146956d46ca
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-24 14:12:52 +0100

    Add LibraryExplorerToolbar

---
+++ PictureLibraryWPF/CustomControls/LibraryExplorerToolbar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PictureLibraryWPF.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for LibraryExplorerToolbar.xaml
+    /// </summary>
+    public partial class LibraryExplorerToolbar : UserControl
+    {
+        public LibraryExplorerToolbar(ILibraryExplorerToolboxViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
