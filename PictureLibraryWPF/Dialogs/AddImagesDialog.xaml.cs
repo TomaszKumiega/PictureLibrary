0b6c2fbe3e600766c8ebbb167741b37ef85ca782
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-26 07:49:23 +0100

    Add AddImagesDialog

---
+++ PictureLibraryWPF/Dialogs/AddImagesDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryViewModel.ViewModel.DialogViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PictureLibraryWPF.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for AddImagesDialog.xaml
+    /// </summary>
+    public partial class AddImagesDialog : Window
+    {
+        public AddImagesDialog(IAddImagesDialogViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+
+        private void LibrariesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var viewModel = DataContext as IAddImagesDialogViewModel;
+            var library = (LibrariesListView.SelectedItem as ListViewItem).DataContext as Library;
+
+            viewModel.SelectedLibrary = library;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
