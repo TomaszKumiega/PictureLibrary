a0c11b678d0ab42cee40c0c21a956e9a06e46dfe
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-26 07:11:57 +0100

    Add AddImagesDialogViewModel

---
+++ PictureLibraryViewModel/ViewModel/DialogViewModels/AddImagesDialogViewModel.cs
@@ -0,0 +1,39 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Repositories;
+using PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryViewModel.ViewModel.DialogViewModels
+{
+    public class AddImagesDialogViewModel : IAddImagesDialogViewModel
+    {
+        private ILibraryExplorerViewModel CommonViewModel { get; }
+        private List<ImageFile> SelectedImages { get; }
+        private IRepository<Library> LibraryRepository { get; }
+
+        public List<Library> Libraries { get; }
+        public Library SelectedLibrary { get; set; }
+
+        public AddImagesDialogViewModel(ILibraryExplorerViewModel commonVM, IRepository<Library> libraryRepository, List<ImageFile> selectedImages)
+        {
+            LibraryRepository = libraryRepository;
+            CommonViewModel = commonVM;
+            SelectedImages = selectedImages;
+
+            Libraries = Task.Run(() => LibraryRepository.GetAllAsync()).Result.ToList();
+        }
+
+        public async Task Add()
+        {
+            // Add images to library folder
+
+            SelectedLibrary.Images.AddRange(SelectedImages);
+            await LibraryRepository.UpdateAsync(SelectedLibrary);
+            await CommonViewModel.LoadCurrentlyShownElements();
+        }
+    }
+}
---
+++ PictureLibraryViewModel/ViewModel/DialogViewModels/IAddImagesDialogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryViewModel.ViewModel.DialogViewModels
+{
+    public interface IAddImagesDialogViewModel
+    {
+        List<Library> Libraries { get; }
+
+        Task Add();
+    }
+}
