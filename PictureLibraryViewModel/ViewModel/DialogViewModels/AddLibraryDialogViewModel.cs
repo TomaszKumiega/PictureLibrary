bc0793e0446e4ac71ac68030699d7245adbf8695
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-27 11:53:10 +0100

    Add AddLibraryDialogViewModel

---
+++ PictureLibraryViewModel/ViewModel/DialogViewModels/AddLibraryDialogViewModel.cs
@@ -0,0 +1,71 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Model.RemoteServer;
+using PictureLibraryModel.Repositories;
+using PictureLibraryModel.Services.ConnectedServicesInfoProvider;
+using PictureLibraryViewModel.ViewModel.Events;
+using PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PictureLibraryViewModel.ViewModel.DialogViewModels
+{
+    public class AddLibraryDialogViewModel : IAddLibraryDialogViewModel
+    {
+        private ILibraryExplorerViewModel _commonViewModel;
+        private IRepository<Library> _libraryRepository;
+        private IConnectedServicesInfoProviderService _connectedServices;
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string FullName { get; set; }
+        public Origin? Origin { get; set; }
+        public List<Origin> Origins { get => _connectedServices.GetAllAvailableOrigins(); }
+        public List<User> Owners { get; }
+
+        public event InvalidInputEventHandler InvalidInput;
+
+        public AddLibraryDialogViewModel(ILibraryExplorerViewModel commonVM, IRepository<Library> libraryRepository, IConnectedServicesInfoProviderService connectedServices)
+        {
+            _commonViewModel = commonVM;
+            _libraryRepository = libraryRepository;
+            _connectedServices = connectedServices;
+        }
+
+        private bool IsNameValid()
+        {
+            if (!Regex.Match(Name, "^[A-Z][a-zA-Z]*$").Success)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsOriginValid()
+        {
+            if (Origin == null) return false;
+            else return true;
+        }
+
+        public async Task AddAsync()
+        {
+            if(!IsNameValid()) InvalidInput?.Invoke(this, new InvalidInputEventArgs("Name"));
+            if (!IsOriginValid()) InvalidInput?.Invoke(this, new InvalidInputEventArgs("Origin"));
+
+            var library = new Library();
+
+            library.Name = Name;
+            if (Description == null) library.Description = "";
+            else library.Description = Description;
+            library.Origin = Origin.Value;
+
+            if (Origin == PictureLibraryModel.Model.Origin.Local) library.FullName = FullName;
+
+            await _libraryRepository.AddAsync(library);
+            await _commonViewModel.LoadCurrentlyShownElements();
+        }
+    }
+}
---
+++ PictureLibraryViewModel/ViewModel/DialogViewModels/IAddLibraryDialogViewModel.cs
@@ -0,0 +1,24 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Model.RemoteServer;
+using PictureLibraryViewModel.ViewModel.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryViewModel.ViewModel.DialogViewModels
+{
+    public interface IAddLibraryDialogViewModel
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        string FullName { get; set; }
+        Origin? Origin { get; set; }
+        List<Origin> Origins { get; }
+        List<User> Owners { get; }
+
+        event InvalidInputEventHandler InvalidInput;
+
+        Task AddAsync();
+    }
+}
