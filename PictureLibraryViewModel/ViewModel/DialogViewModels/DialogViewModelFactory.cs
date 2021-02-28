f73dc2d48836282bc3ef8b991fae08a658928246
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-28 07:08:19 +0100

    Add DialogViewModelFactory

---
+++ PictureLibraryViewModel/ViewModel/DialogViewModels/DialogViewModelFactory.cs
@@ -0,0 +1,38 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Repositories.LibraryRepositories;
+using PictureLibraryModel.Services.ConnectedServicesInfoProvider;
+using PictureLibraryViewModel.Commands;
+using PictureLibraryViewModel.ViewModel.LibraryExplorerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.DialogViewModels
+{
+    public class DialogViewModelFactory : IDialogViewModelFactory
+    {
+        private ILibraryExplorerViewModel _commonViewModel;
+        private IConnectedServicesInfoProviderService _connectedServices;
+
+        public DialogViewModelFactory(ILibraryExplorerViewModel commonVM, IConnectedServicesInfoProviderService connectedServices)
+        {
+            _commonViewModel = commonVM;
+            _connectedServices = connectedServices
+        }
+
+        public IAddLibraryDialogViewModel GetAddLibraryDialogViewModel()
+        {
+            return new AddLibraryDialogViewModel(_commonViewModel, new LibraryRepository(_connectedServices, new LibraryRepositoriesFactory()), _connectedServices);
+        }
+
+        public IAddTagDialogViewModel GetAddTagDialogViewModel()
+        {
+            return new AddTagDialogViewModel(_commonViewModel, new CommandFactory());
+        }
+
+        public IAddImagesDialogViewModel GetImagesDialogViewModel(List<ImageFile> selectedImages)
+        {
+            return new AddImagesDialogViewModel(_commonViewModel, new LibraryRepository(_connectedServices, new LibraryRepositoriesFactory()), selectedImages, new CommandFactory());
+        }
+    }
+}
---
+++ PictureLibraryViewModel/ViewModel/DialogViewModels/IDialogViewModelFactory.cs
@@ -0,0 +1,14 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.DialogViewModels
+{
+    public interface IDialogViewModelFactory
+    {
+        IAddTagDialogViewModel GetAddTagDialogViewModel();
+        IAddLibraryDialogViewModel GetAddLibraryDialogViewModel();
+        IAddImagesDialogViewModel GetImagesDialogViewModel(List<ImageFile> selectedImages);
+    }
+}
