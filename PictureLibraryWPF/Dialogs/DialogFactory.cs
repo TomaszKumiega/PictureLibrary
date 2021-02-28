f2b3ee9ddf2c02cde548494faf44112ce56753cd
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-02-28 07:47:36 +0100

    Add DialogFactory

---
+++ PictureLibraryWPF/Dialogs/DialogFactory.cs
@@ -0,0 +1,33 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryViewModel.ViewModel.DialogViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryWPF.Dialogs
+{
+    public class DialogFactory : IDialogFactory
+    {
+        private IDialogViewModelFactory _viewModelFactory;
+
+        public DialogFactory(IDialogViewModelFactory viewModelFactory)
+        {
+            _viewModelFactory = viewModelFactory;
+        }
+
+        public AddImagesDialog GetAddImagesDialog(List<ImageFile> selectedImages)
+        {
+            return new AddImagesDialog(_viewModelFactory.GetImagesDialogViewModel(selectedImages));
+        }
+
+        public AddLibraryDialog GetAddLibraryDialog()
+        {
+            return new AddLibraryDialog(_viewModelFactory.GetAddLibraryDialogViewModel());
+        }
+
+        public AddTagDialog GetAddTagDialog()
+        {
+            return new AddTagDialog(_viewModelFactory.GetAddTagDialogViewModel());
+        }
+    }
+}
---
+++ PictureLibraryWPF/Dialogs/IDialogFactory.cs
@@ -0,0 +1,14 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryWPF.Dialogs
+{
+    public interface IDialogFactory
+    {
+        AddImagesDialog GetAddImagesDialog(List<ImageFile> selectedFiles);
+        AddLibraryDialog GetAddLibraryDialog();
+        AddTagDialog GetAddTagDialog();
+    }
+}
