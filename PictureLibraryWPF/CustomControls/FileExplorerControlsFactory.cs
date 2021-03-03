b6ab36c1199b5bde343690bf5f967d194a55c5f1
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-03-03 12:20:24 +0100

    Refactor FileExplorerControlsFactory

--- PictureLibraryWPF/CustomControls/FileExplorerControlsFactory.cs
+++ PictureLibraryWPF/CustomControls/FileExplorerControlsFactory.cs
@@ -3,8 +3,10 @@ using PictureLibraryModel.Services.FileSystemServices;
 using PictureLibraryViewModel;
 using PictureLibraryViewModel.Helpers;
 using PictureLibraryViewModel.ViewModel;
+using PictureLibraryViewModel.ViewModel.DialogViewModels;
 using PictureLibraryViewModel.ViewModel.FileExplorerViewModels;
 using PictureLibraryWPF.Clipboard;
+using PictureLibraryWPF.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,30 +16,28 @@ namespace PictureLibraryWPF.CustomControls
 {
     public class FileExplorerControlsFactory : IFileExplorerControlsFactory
     {
-        private IExplorableElementsViewViewModel _filesViewViewModel;
-        private IFileExplorerToolboxViewModel _filesToolboxViewModel;
-        private IExplorableElementsTreeViewModel _filesTreeViewModel;
+        private IFileExplorerViewModelFactory _fileExplorerViewModelFactory;
+        private IDialogViewModelFactory _dialogViewModelFactory;
 
-        public FileExplorerControlsFactory(IFileExplorerViewModelFactory factory)
+        public FileExplorerControlsFactory(IFileExplorerViewModelFactory fileExplorerViewModelFactory, IDialogViewModelFactory dialogViewModelFactory)
         {
-            _filesViewViewModel = factory.GetFilesViewViewModel();
-            _filesToolboxViewModel = factory.GetFileToolboxViewModel(new WPFClipboard());
-            _filesTreeViewModel = factory.GetFileTreeViewModel();
+            _fileExplorerViewModelFactory = fileExplorerViewModelFactory;
+            _dialogViewModelFactory = dialogViewModelFactory;
         }
 
         public ElementsTree GetFileElementsTree()
         {
-            return new ElementsTree(_filesTreeViewModel);
+            return new ElementsTree(_fileExplorerViewModelFactory.GetFileTreeViewModel());
         }
 
         public ElementsView GetFileElementsView()
         {
-            return new ElementsView(_filesViewViewModel);
+            return new ElementsView(_fileExplorerViewModelFactory.GetFilesViewViewModel());
         }
 
         public FileExplorerToolbar GetFileExplorerToolbar()
         {
-            return new FileExplorerToolbar(_filesToolboxViewModel);
+            return new FileExplorerToolbar(_fileExplorerViewModelFactory.GetFileToolboxViewModel(new WPFClipboard()), new DialogFactory(_dialogViewModelFactory));
         }
     }
 }
