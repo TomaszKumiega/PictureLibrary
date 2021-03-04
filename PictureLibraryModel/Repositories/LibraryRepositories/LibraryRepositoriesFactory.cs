5ac12a027ae4388031305a44b08be04a88aa2cbb
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-03-04 14:59:09 +0100

    Change LocalLibraryRepository factory method

--- PictureLibraryModel/Repositories/LibraryRepositories/LibraryRepositoriesFactory.cs
+++ PictureLibraryModel/Repositories/LibraryRepositories/LibraryRepositoriesFactory.cs
@@ -1,5 +1,6 @@
 ﻿using PictureLibraryModel.Model;
 using PictureLibraryModel.Services.FileSystemServices;
+using PictureLibraryModel.Services.SettingsProvider;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +9,16 @@ namespace PictureLibraryModel.Repositories.LibraryRepositories
 {
     public class LibraryRepositoriesFactory : ILibraryRepositoriesFactory
     {
+        private ISettingsProviderService SettingsProvider { get; }
+
+        public LibraryRepositoriesFactory(ISettingsProviderService settingsProvider)
+        {
+            SettingsProvider = settingsProvider;
+        }
+
         public IRepository<Library> GetLocalLibraryRepository()
         {
-            return new LocalLibraryRepository(new FileService(), new DirectoryService());
+            return new LocalLibraryRepository(new FileService(), new DirectoryService(), SettingsProvider);
         }
     }
 }
