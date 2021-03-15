ed341901d5930a55cb8cd4e96f4826ff7c09be70
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-03-15 15:19:33 +0100

    Add loading library page

--- PictureLibraryWPF/MainWindow.xaml.cs
+++ PictureLibraryWPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using PictureLibraryWPF.CustomControls;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -22,12 +23,14 @@ namespace PictureLibraryWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private IFileExplorerControlsFactory _controlsFactory;
+        private IFileExplorerControlsFactory _fileExplorerControlsFactory;
+        private ILibraryExplorerControlsFactory _libraryExplorerControlsFactory;
         private List<Control> CurrentPageControls { get; }
 
-        public MainWindow(IFileExplorerControlsFactory controlsFactory)
+        public MainWindow(IFileExplorerControlsFactory fileExplorerControlsFactory, ILibraryExplorerControlsFactory libraryExplorerControlsFactory)
         {
-            _controlsFactory = controlsFactory;
+            _fileExplorerControlsFactory = fileExplorerControlsFactory;
+            _libraryExplorerControlsFactory = libraryExplorerControlsFactory;
             CurrentPageControls = new List<Control>();
 
             InitializeComponent();
@@ -54,7 +57,7 @@ namespace PictureLibraryWPF
             ToolBarShadow.Fill = new SolidColorBrush(Color.FromArgb(29, 31, 33, 1)); // change rectangle color to #2b2d30
 
             // Add files tree to the grid
-            var filesTree = _controlsFactory.GetFileElementsTree();
+            var filesTree = _fileExplorerControlsFactory.GetFileElementsTree();
             Grid.Children.Add(filesTree);
             filesTree.HorizontalAlignment = HorizontalAlignment.Stretch;
             filesTree.VerticalAlignment = VerticalAlignment.Stretch;
@@ -62,7 +65,7 @@ namespace PictureLibraryWPF
             Grid.SetRow(filesTree, 8);
 
             // Add files view to the grid
-            var filesView = _controlsFactory.GetFileElementsView();
+            var filesView = _fileExplorerControlsFactory.GetFileElementsView();
             MainPanelGrid.Children.Add(filesView);
             filesView.HorizontalAlignment = HorizontalAlignment.Stretch;
             filesView.VerticalAlignment = VerticalAlignment.Stretch;
@@ -71,7 +74,7 @@ namespace PictureLibraryWPF
             Grid.SetColumnSpan(filesView, 2);
 
             // Add file explorer toolbar to the grid
-            var toolbar = _controlsFactory.GetFileExplorerToolbar();
+            var toolbar = _fileExplorerControlsFactory.GetFileExplorerToolbar();
             MainPanelGrid.Children.Add(toolbar);
             toolbar.HorizontalAlignment = HorizontalAlignment.Stretch;
             toolbar.VerticalAlignment = VerticalAlignment.Stretch;
@@ -84,6 +87,47 @@ namespace PictureLibraryWPF
             CurrentPageControls.Add(toolbar);
         }
 
+        private async Task LoadLibraryExplorerPage()
+        {
+            // Reset window to default state
+            ResetButtonClickedRectangles();
+            RemoveCurrentPageControlsFromTheGrid();
+
+            LibrariesButtonClickedRectangle.Fill = new SolidColorBrush(Color.FromArgb(0, 102, 255, 1)); // change rectangle color to #0066ff
+            ToolBarShadow.Fill = new SolidColorBrush(Color.FromArgb(29, 31, 33, 1)); // change rectangle color to #2b2d30
+
+
+            // Add libraries tree to the grid
+            var librariesTree = await _libraryExplorerControlsFactory.GetLibrariesTree();
+            Grid.Children.Add(librariesTree);
+            librariesTree.HorizontalAlignment = HorizontalAlignment.Stretch;
+            librariesTree.VerticalAlignment = VerticalAlignment.Stretch;
+            Grid.SetColumn(librariesTree, 0);
+            Grid.SetRow(librariesTree, 8);
+
+            // Add libraries view to the grid
+            var librariesView = _libraryExplorerControlsFactory.GetLibrariesView();
+            MainPanelGrid.Children.Add(librariesView);
+            librariesView.HorizontalAlignment = HorizontalAlignment.Stretch;
+            librariesView.VerticalAlignment = VerticalAlignment.Stretch;
+            Grid.SetColumn(librariesView, 0);
+            Grid.SetRow(librariesView, 2);
+            Grid.SetColumnSpan(librariesView, 2);
+
+            // Add libraries toolbar to the grid
+            var librariesToolbar = _libraryExplorerControlsFactory.GetLibraryExplorerToolbar();
+            MainPanelGrid.Children.Add(librariesToolbar);
+            librariesToolbar.HorizontalAlignment = HorizontalAlignment.Stretch;
+            librariesToolbar.VerticalAlignment = VerticalAlignment.Stretch;
+            Grid.SetColumn(librariesToolbar, 0);
+            Grid.SetRow(librariesToolbar, 0);
+            Grid.SetColumnSpan(librariesToolbar, 2);
+
+            CurrentPageControls.Add(librariesTree);
+            CurrentPageControls.Add(librariesToolbar);
+            CurrentPageControls.Add(librariesToolbar);
+        }
+
         private void RemoveCurrentPageControlsFromTheGrid()
         {
             foreach(var t in CurrentPageControls)
@@ -91,6 +135,8 @@ namespace PictureLibraryWPF
                 Grid.Children.Remove(t);
                 MainPanelGrid.Children.Remove(t);
             }
+
+            CurrentPageControls.Clear();
         }
 
         private void ResetButtonClickedRectangles()
@@ -152,9 +198,9 @@ namespace PictureLibraryWPF
             LoadFileExplorerPage();
         }
 
-        private void LibrariesButton_Click(object sender, RoutedEventArgs e)
+        private async void LibrariesButton_Click(object sender, RoutedEventArgs e)
         {
-
+            await LoadLibraryExplorerPage();
         }
 
         private void SettingsButton_Click(object sender, RoutedEventArgs e)
