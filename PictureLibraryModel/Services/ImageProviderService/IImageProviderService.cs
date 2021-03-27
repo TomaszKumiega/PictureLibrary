068668f45ddb139c9e9e23e7031636dd889ca9b8
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-03-27 16:03:54 +0100

    Add IImageProviderService interface

---
+++ PictureLibraryModel/Services/ImageProviderService/IImageProviderService.cs
@@ -0,0 +1,17 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryModel.Services.ImageProviderService
+{
+    public interface IImageProviderService
+    {
+        Task AddImageToLibraryAsync(ImageFile imageFile, byte[] image);
+        Task<byte[]> GetImageAsync(ImageFile imageFile);
+        Task UpdateImageAsync(ImageFile imageFile, byte[] image);
+        Task LoadImagesIconsAsync(IEnumerable<ImageFile> imageFiles);
+        Task RemoveImageAsync(ImageFile imageFile);
+    }
+}
