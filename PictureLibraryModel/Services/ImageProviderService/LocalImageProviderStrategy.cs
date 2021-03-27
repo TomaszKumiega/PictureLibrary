6c03e7b9e1defb45169ff18a33b59300943ce06d
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-03-27 20:46:43 +0100

    Add LocalImageProviderStrategy

---
+++ PictureLibraryModel/Services/ImageProviderService/LocalImageProviderStrategy.cs
@@ -0,0 +1,44 @@
+﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Services.FileSystemServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryModel.Services.ImageProviderService
+{
+    public class LocalImageProviderStrategy : IImageProviderStrategy
+    {
+        private IFileService FileService { get; }
+
+        public LocalImageProviderStrategy(IFileService fileService)
+        {
+            FileService = fileService;
+        }
+
+        public Task AddImageToLibraryAsync(ImageFile imageFile, byte[] image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> GetImageAsync(ImageFile imageFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LoadImagesIconsAsync(IEnumerable<ImageFile> imageFiles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveImageAsync(ImageFile imageFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateImageAsync(ImageFile imageFile, byte[] image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
