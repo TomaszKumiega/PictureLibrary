2d18f58708597209e6f5243ed21d2d7af6db32c8
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-02 16:48:58 +0200

    Add ImageProviderContext

---
+++ PictureLibraryModel/Services/ImageProviderService/IImageProviderContext.cs
@@ -0,0 +1,19 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryModel.Services.ImageProviderService
+{
+    public interface IImageProviderContext
+    {
+        IImageProviderStrategy Strategy { get; set; }
+
+        Task<ImageFile> AddImageToLibraryAsync(ImageFile imageFile, string libraryDirectory);
+        Task<byte[]> GetImageAsync(ImageFile imageFile);
+        Task UpdateImageAsync(ImageFile imageFile, byte[] image);
+        Task LoadImagesIconsAsync(IEnumerable<ImageFile> imageFiles);
+        Task RemoveImageAsync(ImageFile imageFile);
+    }
+}
---
+++ PictureLibraryModel/Services/ImageProviderService/ImageProviderContext.cs
@@ -0,0 +1,38 @@
+﻿using PictureLibraryModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLibraryModel.Services.ImageProviderService
+{
+    public class ImageProviderContext : IImageProviderContext
+    {
+        public IImageProviderStrategy Strategy { get; set; }
+
+        public async Task<ImageFile> AddImageToLibraryAsync(ImageFile imageFile, string libraryDirectory)
+        {
+            return await Strategy.AddImageToLibraryAsync(imageFile, libraryDirectory);
+        }
+
+        public async Task<byte[]> GetImageAsync(ImageFile imageFile)
+        {
+            return await Strategy.GetImageAsync(imageFile);
+        }
+
+        public async Task LoadImagesIconsAsync(IEnumerable<ImageFile> imageFiles)
+        {
+            await Strategy.LoadImagesIconsAsync(imageFiles);
+        }
+
+        public async Task RemoveImageAsync(ImageFile imageFile)
+        {
+            await Strategy.RemoveImageAsync(imageFile);
+        }
+
+        public async Task UpdateImageAsync(ImageFile imageFile, byte[] image)
+        {
+            await Strategy.UpdateImageAsync(imageFile, image);
+        }
+    }
+}
