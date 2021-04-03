91c4c18076deeec5f9c565e402b53cd5cd7d2fa4
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-03 18:35:50 +0200

    Add command factory injection to the factory

--- PictureLibraryViewModel/ViewModel/DialogViewModels/DialogViewModelFactory.cs
+++ PictureLibraryViewModel/ViewModel/DialogViewModels/DialogViewModelFactory.cs
@@ -14,34 +14,36 @@ namespace PictureLibraryViewModel.ViewModel.DialogViewModels
     public class DialogViewModelFactory : IDialogViewModelFactory
     {
         private IImageProviderService ImageProviderService { get; }
+        private ICommandFactory CommandFactory { get; }
 
         private ILibraryExplorerViewModel CommonViewModel { get; }
         private IConnectedServicesInfoProviderService ConnectedServices { get; }
         private ILibraryRepositoriesFactory LibraryRepositoriesFactory { get; }
 
         public DialogViewModelFactory(ILibraryExplorerViewModel commonVM, IConnectedServicesInfoProviderService connectedServices, ILibraryRepositoriesFactory libraryRepositoriesFactory, 
-            IImageProviderService imageProviderService)
+            IImageProviderService imageProviderService, ICommandFactory commandFactory)
         {
             CommonViewModel = commonVM;
             ConnectedServices = connectedServices;
             LibraryRepositoriesFactory = libraryRepositoriesFactory;
             ImageProviderService = imageProviderService;
+            CommandFactory = commandFactory;
 
         }
 
         public IAddLibraryDialogViewModel GetAddLibraryDialogViewModel()
         {
-            return new AddLibraryDialogViewModel(CommonViewModel, new LibraryRepository(ConnectedServices, LibraryRepositoriesFactory), ConnectedServices, new CommandFactory());
+            return new AddLibraryDialogViewModel(CommonViewModel, new LibraryRepository(ConnectedServices, LibraryRepositoriesFactory), ConnectedServices, CommandFactory);
         }
 
         public IAddTagDialogViewModel GetAddTagDialogViewModel()
         {
-            return new AddTagDialogViewModel(CommonViewModel, new CommandFactory());
+            return new AddTagDialogViewModel(CommonViewModel, CommandFactory);
         }
 
         public async Task<IAddImagesDialogViewModel> GetImagesDialogViewModel(List<ImageFile> selectedImages)
         {
-            var viewModel = new AddImagesDialogViewModel(CommonViewModel, new LibraryRepository(ConnectedServices, LibraryRepositoriesFactory), selectedImages, new CommandFactory(), ImageProviderService);
+            var viewModel = new AddImagesDialogViewModel(CommonViewModel, new LibraryRepository(ConnectedServices, LibraryRepositoriesFactory), selectedImages, CommandFactory, ImageProviderService);
             await viewModel.Initialize();
 
             return viewModel;
