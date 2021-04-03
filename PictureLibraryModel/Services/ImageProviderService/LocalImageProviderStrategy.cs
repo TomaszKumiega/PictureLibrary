e655a7f4b09ae3cace063d11582e44eab45aca35
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-03 14:47:42 +0200

    Add LocalImageProviderStrategy

--- PictureLibraryModel/Services/ImageProviderService/LocalImageProviderStrategy.cs
+++ PictureLibraryModel/Services/ImageProviderService/LocalImageProviderStrategy.cs
@@ -1,7 +1,9 @@
 ﻿using PictureLibraryModel.Model;
+using PictureLibraryModel.Model.Builders.ImageFileBuilder;
 using PictureLibraryModel.Services.FileSystemServices;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +12,49 @@ namespace PictureLibraryModel.Services.ImageProviderService
     public class LocalImageProviderStrategy : IImageProviderStrategy
     {
         private IFileService FileService { get; }
+        private IImageFileBuilder ImageFileBuilder { get; }
 
-        public LocalImageProviderStrategy(IFileService fileService)
+        public LocalImageProviderStrategy(IFileService fileService, IImageFileBuilder imageFileBuilder)
         {
             FileService = fileService;
+            ImageFileBuilder = imageFileBuilder;
         }
 
-        public Task AddImageToLibraryAsync(ImageFile imageFile, byte[] image)
+        public async Task<ImageFile> AddImageToLibraryAsync(ImageFile imageFile, string libraryFullName)
         {
-            throw new NotImplementedException();
+            Model.Directory directory;
+
+            try
+            {
+                directory = FileService.GetParent(libraryFullName);
+            }
+            catch(Exception)
+            {
+                throw new Exception("Error finding library directory: " + libraryFullName);
+            }
+
+            var path = directory + imageFile.Name;
+
+            await Task.Run(() => FileService.Copy(imageFile.FullName, path));
+
+            var fileInfo = new FileInfo(path);
+           
+            var newImageFile =
+                ImageFileBuilder
+                .StartBuilding()
+                .WithName(fileInfo.Name)
+                .WithFullName(fileInfo.FullName)
+                .WithExtension(fileInfo.Extension)
+                .WithCreationTime(fileInfo.CreationTime)
+                .WithLastAccessTime(fileInfo.LastAccessTime)
+                .WithLastWriteTime(fileInfo.LastWriteTime)
+                .WithLibraryFullName(libraryFullName)
+                .WithSize(fileInfo.Length)
+                .WithTags(new List<Tag>())
+                .From(Origin.Local)
+                .Build();
+
+            return newImageFile;
         }
 
         public async Task<byte[]> GetImageAsync(ImageFile imageFile)
@@ -36,9 +72,9 @@ namespace PictureLibraryModel.Services.ImageProviderService
             await Task.Run(() => FileService.Remove(imageFile.FullName));
         }
 
-        public Task UpdateImageAsync(ImageFile imageFile, byte[] image)
+        public async Task UpdateImageAsync(ImageFile imageFile, byte[] image)
         {
-            throw new NotImplementedException();
+            await Task.Run(() => FileService.WriteAllBytes(imageFile.FullName, image));
         }
     }
 }
