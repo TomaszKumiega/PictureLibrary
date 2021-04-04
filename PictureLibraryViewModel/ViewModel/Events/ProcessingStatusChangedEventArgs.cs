50f37c9da2ba60dc4e5d43247f61a7ea04c8cf6e
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-04 12:37:48 +0200

    Add ProcessingStatusChanged event

---
+++ PictureLibraryViewModel/ViewModel/Events/ProcessingStatusChangedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryViewModel.ViewModel.Events
+{
+    public enum ProcessingStatus
+    {
+        Processing,
+        Finished,
+        Failed
+    }
+
+    public class ProcessingStatusChangedEventArgs
+    {
+        public ProcessingStatus Status { get; }
+
+        public ProcessingStatusChangedEventArgs(ProcessingStatus status)
+        {
+            Status = status;
+        }
+    }
+
+    public delegate void ProcessingStatusChangedEventHandler(object sender, ProcessingStatusChangedEventArgs args);
+}
+}
