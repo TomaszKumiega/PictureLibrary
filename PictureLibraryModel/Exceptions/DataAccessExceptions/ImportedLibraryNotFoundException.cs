7030313df58ab1fd37984160382618807182cde4
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-08-31 21:58:51 +0200

    Add data access custom exceptions

---
+++ PictureLibraryModel/Exceptions/DataAccessExceptions/ImportedLibraryNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PictureLibraryModel.Exceptions.DataAccessExceptions
+{
+    public class ImportedLibraryNotFoundException : FileNotFoundException
+    {
+        public ImportedLibraryNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public ImportedLibraryNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ PictureLibraryModel/Exceptions/DataAccessExceptions/LibraryFileIOException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PictureLibraryModel.Exceptions.DataAccessExceptions
+{
+    public class LibraryFileIOException : IOException
+    {
+        public LibraryFileIOException(string message) : base(message)
+        {
+
+        }
+
+        public LibraryFileIOException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
