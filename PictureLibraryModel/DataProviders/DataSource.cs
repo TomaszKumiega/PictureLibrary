919b0f43e81fa572e48481e6846f5a65096d2230
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-09-07 21:39:59 +0200

    Add data source

---
+++ PictureLibraryModel/DataProviders/DataSource.cs
@@ -0,0 +1,15 @@
+﻿using PictureLibraryModel.Model.RemoteStorages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.DataProviders
+{
+    public class DataSource : IDataSource
+    {
+        public bool IsLocalDataSource { get; }
+        public IRemoteStorageInfo RemoteStorageInfo { get; }
+        public IImageProvider ImageProvider { get; }
+        public ILibraryProvider LibraryProvider { get; }
+    }
+}
---
+++ PictureLibraryModel/DataProviders/IDataSource.cs
@@ -0,0 +1,12 @@
+﻿using PictureLibraryModel.Model.RemoteStorages;
+
+namespace PictureLibraryModel.DataProviders
+{
+    public interface IDataSource
+    {
+        IImageProvider ImageProvider { get; }
+        bool IsLocalDataSource { get; }
+        ILibraryProvider LibraryProvider { get; }
+        IRemoteStorageInfo RemoteStorageInfo { get; }
+    }
+}
