a99fd2d1d624b551c149aec6178b2d4b5b5cc901
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-12-11 18:36:17 +0100

    Refactor DataSource properties

---
+++ PictureLibraryModel/DataProviders/DataSource/DataSource.cs
@@ -0,0 +1,14 @@
+﻿using PictureLibraryModel.Model.RemoteStorages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.DataProviders
+{
+    public class DataSource : IDataSource
+    {
+        public IRemoteStorageInfo RemoteStorageInfo { get; set; }
+        public IImageFileProvider ImageProvider { get; set; }
+        public ILibraryProvider LibraryProvider { get; set; }
+    }
+}
---
+++ PictureLibraryModel/DataProviders/DataSource/IDataSource.cs
@@ -0,0 +1,11 @@
+﻿using PictureLibraryModel.Model.RemoteStorages;
+
+namespace PictureLibraryModel.DataProviders
+{
+    public interface IDataSource
+    {
+        IImageFileProvider ImageProvider { get; set; }
+        ILibraryProvider LibraryProvider { get; set; }
+        IRemoteStorageInfo RemoteStorageInfo { get; set; }
+    }
+}
