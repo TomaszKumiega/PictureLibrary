9a49d00d0a3cfeb24f79a3a948407c5ec5b5d0b5
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-12-11 18:37:10 +0100

    Add DataSource builder

---
+++ PictureLibraryModel/DataProviders/Builders/IDataSourceBuilder.cs
@@ -0,0 +1,12 @@
+﻿using PictureLibraryModel.Model.RemoteStorages;
+
+namespace PictureLibraryModel.DataProviders.Builders
+{
+    public interface IDataSourceBuilder
+    {
+        IDataSourceBuilder CreateLibraryProvider();
+        IDataSourceBuilder CreateImageFileProvider();
+        IDataSourceBuilder WithRemoteStorageInfo(IRemoteStorageInfo remoteStorageInfo);
+        IDataSource Build();
+    }
+}
---
+++ PictureLibraryModel/DataProviders/Builders/LocalDataSourceBuilder.cs
@@ -0,0 +1,44 @@
+﻿using PictureLibraryModel.Model.RemoteStorages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.DataProviders.Builders
+{
+    public class LocalDataSourceBuilder : IDataSourceBuilder
+    {
+        private IDataSource DataSource { get; }
+        private Func<LocalLibraryProvider> LocalLibraryProviderLocator { get; }
+        private Func<LocalImageFileProvider> LocalImageFileProviderLocator { get; }
+
+        public LocalDataSourceBuilder(IDataSource dataSource, Func<LocalImageFileProvider> localImageFileProviderLocator, Func<LocalLibraryProvider> localLibraryProviderLocator)
+        {
+            DataSource = dataSource;
+            LocalImageFileProviderLocator = localImageFileProviderLocator;
+            LocalLibraryProviderLocator = localLibraryProviderLocator;
+        }
+
+        public IDataSourceBuilder CreateImageFileProvider()
+        {
+            DataSource.ImageProvider = LocalImageFileProviderLocator();
+            return this;
+        }
+
+        public IDataSourceBuilder CreateLibraryProvider()
+        {
+            DataSource.LibraryProvider = LocalLibraryProviderLocator();
+            return this;
+        }
+
+        public IDataSourceBuilder WithRemoteStorageInfo(IRemoteStorageInfo remoteStorageInfo)
+        {
+            DataSource.RemoteStorageInfo = remoteStorageInfo;
+            return this;
+        }
+
+        public IDataSource Build()
+        {
+            return DataSource;
+        }
+    }
+}
