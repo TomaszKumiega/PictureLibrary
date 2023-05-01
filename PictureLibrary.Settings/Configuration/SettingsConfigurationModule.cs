11681d3b803e70ce308175876743d2111d54da8e
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-05-02 01:25:42 +0200

    Add SettingsConfigurationModule

---
+++ PictureLibrary.Settings/Configuration/SettingsConfigurationModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace PictureLibrary.Settings.Configuration
+{
+    public class SettingsConfigurationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(GetType().Assembly).AsImplementedInterfaces().SingleInstance();
+        }
+    }
+}
