aad2eb117fe717390895c2dfd954a18baabf900e
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-05-04 20:28:31 +0200

    Add DirectoryService

---
+++ PictureLibrary.FileSystem.API/Directories/DirectoryService.cs
@@ -0,0 +1,17 @@
+﻿namespace PictureLibrary.FileSystem.API.Directories
+{
+    public class DirectoryService : IDirectoryService
+    {
+        #region Public methods
+        public void Create(string path)
+        {
+            _ = Directory.CreateDirectory(path);
+        }
+
+        public DirectoryInfo GetDirectoryInfo(string path)
+        {
+            return new DirectoryInfo(path);
+        }
+        #endregion
+    }
+}
