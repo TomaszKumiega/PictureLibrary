6af4a5e35bafa1e77736f1a3d795bfdd692c2308
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-05-04 20:36:50 +0200

    Add PathFinder implementation

---
+++ PictureLibrary.FileSystem.API/PathFinder.cs
@@ -0,0 +1,12 @@
+﻿namespace PictureLibrary.FileSystem.API
+{
+    public class PathFinder : IPathFinder
+    {
+        public string AppFolderPath => GetType().Assembly.Location;
+
+        public string GetDataStoreInfoFilePath(Type typeOfDataStoreInfo)
+        {
+            return AppFolderPath + Path.PathSeparator + $"{typeOfDataStoreInfo.Name}.dsi";
+        }
+    }
+}
