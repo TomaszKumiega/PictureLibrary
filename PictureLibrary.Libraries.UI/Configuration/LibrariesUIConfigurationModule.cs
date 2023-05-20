58339897d940ede293ef7a30c9114dc3e8794f26
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-05-20 19:52:30 +0200

    Add Libraries UI configuration module

---
+++ PictureLibrary.Libraries.UI/Configuration/LibrariesUIConfigurationModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using PictureLibrary.Libraries.UI.ViewModels;
+
+namespace PictureLibrary.Libraries.UI.Configuration
+{
+    public class LibrariesUIConfigurationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<LibrariesPageViewModel>().AsSelf().SingleInstance();
+        }
+    }
+}
