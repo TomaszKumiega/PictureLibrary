3a7a57212d21b4b8170619cb85b070ccc5bf6de3
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-05-20 19:50:38 +0200

    Add view model to libraries page

--- PictureLibrary.Libraries.UI/MainPage/LibrariesPage.xaml.cs
+++ PictureLibrary.Libraries.UI/MainPage/LibrariesPage.xaml.cs
@@ -1,9 +1,12 @@
+using PictureLibrary.Libraries.UI.ViewModels;
+
 namespace PictureLibrary.Libraries.UI.MainPage;
 
 public partial class LibrariesPage : ContentPage
 {
-	public LibrariesPage()
+	public LibrariesPage(LibrariesPageViewModel librariesPageViewModel)
 	{
+		BindingContext = librariesPageViewModel;
 		InitializeComponent();
 	}
 }
