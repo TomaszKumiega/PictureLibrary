209b974363b78e4e0f74f6a1c826f875c8719960
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-05-26 20:59:07 +0200

    Add Infrastructure UI project

---
+++ PictureLibrary.Infrastructure.UI/Converters/ByteArrayToImageSourceConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace PictureLibrary.Infrastructure.UI.Converters
+{
+    public class ByteArrayToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is byte[] imageContent)
+            {
+                return ImageSource.FromStream(() => new MemoryStream(imageContent));
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
