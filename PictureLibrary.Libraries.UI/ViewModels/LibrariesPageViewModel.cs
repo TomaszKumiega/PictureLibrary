f2b98de1c3a1d8d699e2f466bb73e499a2401148
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-05-27 00:09:30 +0200

    Add remove library operation

--- PictureLibrary.Libraries.UI/ViewModels/LibrariesPageViewModel.cs
+++ PictureLibrary.Libraries.UI/ViewModels/LibrariesPageViewModel.cs
@@ -2,19 +2,34 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using PictureLibrary.DataAccess;
+using PictureLibrary.DataAccess.DataStoreInfos;
+using PictureLibrary.DataAccess.LibraryService;
+using PictureLibrary.Infrastructure.ImplementationSelector;
+using PictureLibrary.Infrastructure.UI;
 using PictureLibrary.Libraries.UI.DataViewModels;
 using PictureLibrary.Libraries.UI.Pages;
+using PictureLibraryModel.Model.DataStoreInfo;
 using System.Collections.ObjectModel;
 
 namespace PictureLibrary.Libraries.UI.ViewModels
 {
     public partial class LibrariesPageViewModel : ObservableObject
     {
+        private readonly IPopupService _popupService;
         private readonly ILibrariesProvider _librariesProvider;
+        private readonly IDataStoreInfoService _dataStoreInfoService;
+        private readonly IImplementationSelector<DataStoreType, ILibraryService> _libraryServiceSelector;
 
-        public LibrariesPageViewModel(ILibrariesProvider librariesProvider)
+        public LibrariesPageViewModel(
+            IPopupService popupService,
+            ILibrariesProvider librariesProvider,
+            IDataStoreInfoService dataStoreInfoService,
+            IImplementationSelector<DataStoreType, ILibraryService> libraryServiceSelector)
         {
+            _popupService = popupService;
             _librariesProvider = librariesProvider;
+            _dataStoreInfoService = dataStoreInfoService;
+            _libraryServiceSelector = libraryServiceSelector;
 
             InitializeViewModelAsync().SafeFireAndForget();
         }
@@ -59,6 +74,24 @@ namespace PictureLibrary.Libraries.UI.ViewModels
         public async Task AddLibrary()
         {
             await Shell.Current.GoToAsync(nameof(AddLibraryPage));
+        }
+
+        [RelayCommand(CanExecute = nameof(CanExecuteOpenLibrary))]
+        public async Task RemoveLibrary()
+        {
+            var dataStoreInfo = _dataStoreInfoService.GetDataStoreInfoFromLibrary(SelectedLibrary.Library);
+            var dataStoreType = dataStoreInfo?.Type ?? DataStoreType.Local;
+            var libraryService = _libraryServiceSelector.Select(dataStoreType);
+
+            bool libraryDeleted = await libraryService.DeleteLibraryAsync(SelectedLibrary.Library);
+
+            if (!libraryDeleted)
+            {
+                _popupService.DisplayAlert("Błąd", "Usuwanie biblioteki nie powiodło się.", "Ok");
+                return;
+            }
+
+            Libraries.Remove(SelectedLibrary);
         }
         #endregion
     }
