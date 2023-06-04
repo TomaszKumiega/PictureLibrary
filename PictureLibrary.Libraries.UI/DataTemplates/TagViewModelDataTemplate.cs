2fba99c9e23ff40e01070ef20d213cb6a13518c0
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-06-04 10:51:12 +0200

    Add TagViewModelDataTemplate

---
+++ PictureLibrary.Libraries.UI/DataTemplates/TagViewModelDataTemplate.cs
@@ -0,0 +1,17 @@
+﻿using PictureLibrary.Libraries.UI.DataViewModels;
+
+namespace PictureLibrary.Libraries.UI.DataTemplates
+{
+    public class TagViewModelDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate TagDataTemplate { get; set; }
+        public DataTemplate CommandDataTemplate { get; set; }
+        
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((TagViewModel)item).IsCommand 
+                ? CommandDataTemplate
+                : TagDataTemplate;
+        }
+    }
+}
