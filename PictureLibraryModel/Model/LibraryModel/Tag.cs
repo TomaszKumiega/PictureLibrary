6264dd54ce58772c7078243ca53c54d912f8c12b
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-06-04 10:50:51 +0200

    Remove unnecessary property

--- PictureLibraryModel/Model/LibraryModel/Tag.cs
+++ PictureLibraryModel/Model/LibraryModel/Tag.cs
@@ -7,7 +7,6 @@ namespace PictureLibraryModel.Model
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public string? Path { get; set; }
         public string? Color { get; set; }
 
         public Tag()
