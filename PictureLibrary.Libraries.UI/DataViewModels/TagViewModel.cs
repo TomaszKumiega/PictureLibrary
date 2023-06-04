9849de1f2054b8e9e1a1526a289f30261ce7dcac
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-06-04 10:51:03 +0200

    Add TagViewModel

---
+++ PictureLibrary.Libraries.UI/DataViewModels/TagViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PictureLibraryModel.Model;
+
+namespace PictureLibrary.Libraries.UI.DataViewModels
+{
+    public class TagViewModel : ObservableObject
+    {
+        public TagViewModel(Tag tag)
+        {
+            Name = tag.Name;
+            Description = tag.Description;
+            Color = string.IsNullOrEmpty(tag.Color)
+                ? Colors.Transparent
+                : Color.FromArgb(tag.Color);
+        }
+
+        public TagViewModel(IRelayCommand command, string commandName, Color color)
+        {
+            RelayCommand = command;
+            CommandName = commandName;
+            Color = color;
+            IsCommand = true;
+        }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public Color Color { get; set; }
+
+        public IRelayCommand? RelayCommand { get; }
+        public string? CommandName { get; }
+        public bool IsCommand { get; }
+    }
+}
